88dcd073a4f7237837654a9e4e8980bdade93b2d
G100my <G100my.lan229@gmail.com>
2021-01-20 13:24:24 +0800

    update eslintrc, add prettierrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,28 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-	env: {
-		browser: true,
-		es2021: true,
-	},
-	extends: ['eslint:recommended', 'plugin:vue/vue3-recommended'],
-	parserOptions: {
-		ecmaVersion: 12,
-		sourceType: 'module',
-	},
-	plugins: ['vue'],
-	rules: {
-		'vue/html-indent': [
-			'error',
-			'tab',
-			{
-				attribute: 1,
-				baseIndent: 1,
-			},
-		],
-		'comma-dangle': ['error', 'only-multiline'],
-	},
-};
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/vue3-recommended',
+    'prettier',
+    'prettier/vue',
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['vue'],
+  rules: {
+    'vue/html-self-closing': [
+      'error',
+      {
+        html: {
+          void: 'any',
+        },
+      },
+    ],
+  },
+}
