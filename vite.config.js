fbe433c658ba208430edb7c578a7ab8fd4dc895c
G100my <G100my.lan229@gmail.com>
2021-01-25 19:53:07 +0800

    init firebase, set vite.config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,9 @@ import vue from '@vitejs/plugin-vue'
  * @type {import('vite').UserConfig}
  */
 export default {
-  plugins: [vue()]
+  plugins: [vue()],
+  optimizeDeps: {
+    include: ['firebase/app', 'firebase/database', 'firebase/analytics'],
+    exclude: ['firebase'],
+  },
 }
