478e9324685678bbfe47fa141c6cc1e9748fd42d
G100my <G100my.lan229@gmail.com>
2021-01-25 19:59:31 +0800

    init vuex

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import store from './store/store.js'
 import spotifyAPI from './plugin/spotify-web-api.js'
 
 // prettier-ignore
 createApp(App)
   .use(router)
+  .use(store)
   .use(spotifyAPI)
   .mount('#app')
