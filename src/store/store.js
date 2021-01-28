a98830c05bf3b30c107654540a692dd901fc0a31
G100my <G100my.lan229@gmail.com>
2021-01-28 18:39:52 +0800

    feat: request data after room_queue changed

--- src/store/store.js
+++ src/store/store.js
@@ -1,16 +1,30 @@
 import { createStore } from 'vuex'
 import firebase from './firebase.js'
+import { spotifyAPI } from '../plugin/spotify-web-api.js'
+import { getImplicitGrantToken } from '../utility/Oauth.js'
 
 // 用不一樣的命名方式做區隔
 const room_queue_ref = firebase.database().ref('room_queue')
 room_queue_ref.on('value', snapshot => {
   store.commit('updateRoomQueue', snapshot.val())
+
+  const trackIdArray = Object.values(snapshot.val()).map(item => item.id)
+
+  if (spotifyAPI.getAccessToken())
+    spotifyAPI.getTracks(trackIdArray).then(result => {
+      store.commit('updateTrackObjectArray', result.tracks)
+    })
+  else {
+    console.log('spotifyAPI AccessToken is null')
+    getImplicitGrantToken()
+  }
 })
 
 const store = createStore({
   state: {
     token: null,
     trackIdQueue: null,
+    trackObjectArray: null,
   },
   getters: {
     getRoomQueueURIArray(state) {
@@ -23,6 +37,9 @@ const store = createStore({
     },
     updateRoomQueue(state, newQueue) {
       state.trackIdQueue = newQueue
+    },
+    updateTrackObjectArray(state, newArray) {
+      state.trackObjectArray = newArray
     },
     room_queue_push(state, addedTrack) {
       room_queue_ref.push(addedTrack.id)
