17a0a6174bfee3dc1b9b86dabb529e9d28440e19
G100my <G100my.lan229@gmail.com>
2021-01-28 18:41:33 +0800

    add Track in RoomQueue

--- src/components/TheRoomQueue.vue
+++ src/components/TheRoomQueue.vue
@@ -1,17 +1,43 @@
 <template>
-  <div class="tracks">
+  <div class="room-queue">
     <h1>Room Tracks</h1>
     <button @click.prevent="testAdd">testAdd</button>
     <button @click.prevent="testRemoveFirst">testRemoveFirst</button>
-    <h2 v-for="(trackID, index) in $store.state.roomQueueID" :key="index">{{ trackID }}</h2>
+    <table>
+      <thead>
+        <tr>
+          <td>Cover</td>
+          <td>Track / Artists</td>
+          <td>Album</td>
+          <td>Detail</td>
+          <td>Feature</td>
+        </tr>
+      </thead>
+      <tbody>
+        <Track v-for="(detailObject, index) in detailObjects" :key="index" :info="detailObject" />
+      </tbody>
+    </table>
   </div>
 </template>
 <script>
+import Track from './Track.vue'
+
 export default {
+  components: {
+    Track,
+  },
   data() {
     return {
       testNumber: 0,
     }
+  },
+  computed: {
+    idQueue() {
+      return this.$store.state.trackIdQueue
+    },
+    detailObjects() {
+      return this.$store.state.trackObjectArray
+    },
   },
   methods: {
     testAdd() {
