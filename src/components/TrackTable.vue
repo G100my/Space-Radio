ce75b34d28852055e95f5db64fcabe87ab7932c1
G100my <G100my.lan229@gmail.com>
2021-01-29 02:03:25 +0800

    refactor: split <table> into TrackTable

---
+++ src/components/TrackTable.vue
@@ -0,0 +1,16 @@
+<template>
+  <table>
+    <thead>
+      <tr>
+        <td>Cover</td>
+        <td>Track / Artists</td>
+        <td>Album</td>
+        <td>Detail</td>
+        <td>Feature</td>
+      </tr>
+    </thead>
+    <tbody>
+      <slot />
+    </tbody>
+  </table>
+</template>
