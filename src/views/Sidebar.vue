57a00ef8cdd1c827cff04073481c3b5fe6c464d1
G100my <G100my.lan229@gmail.com>
2021-02-01 18:09:29 +0800

    feat: init Sidebar

---
+++ src/views/Sidebar.vue
@@ -0,0 +1,182 @@
+<template>
+  <div class="sidebar-container">
+    <h1>Jukebox</h1>
+    <p>Now Playing:</p>
+    <div class="track-info">
+      <div class="cover">
+        <img :src="cover" alt="" />
+      </div>
+      <p class="artists">
+        <a v-for="(artist, index) in artists" :key="index" :href="artist.external_urls.spotify">{{ artist.name }}</a>
+      </p>
+      <p class="album">
+        <a :href="album.external_urls.spotify">{{ album.name }}</a>
+      </p>
+    </div>
+    <div class="control-board">
+      <div class="volumn-control">
+        <img src="../assets/volume.svg" alt="" />
+        <div class="progress">
+          <div class="bar">
+            <p :style="{ width: `${volumeValue}%` }" />
+          </div>
+        </div>
+        <p>{{ volumeValue }}</p>
+        <div class="buttons">
+          <button type="button" @click="turnDown">-</button>
+          <button type="button" @click="turnUp">+</button>
+        </div>
+      </div>
+      <div class="terminate-control">
+        <div class="sign">
+          <span :class="{ active: dislike > 0 }" />
+          <span :class="{ active: dislike > 1 }" />
+        </div>
+        <p>{{ dislike }}</p>
+        <div class="buttons">
+          <button v-show="!isVoted" type="button" @click="increaseDislike">terminate</button>
+          <button v-show="isVoted" type="button" @click="reduceDislike">cancel</button>
+        </div>
+      </div>
+      <div class="collect">
+        <button type="button">collect</button>
+      </div>
+    </div>
+    <div class="log">
+      <ul>
+        <li />
+      </ul>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      isVoted: false,
+    }
+  },
+  computed: {
+    trackInfo() {
+      return this.$store.state.Playing.trackInfo
+    },
+    artists() {
+      return this.trackInfo.artists
+    },
+    album() {
+      return this.trackInfo.album
+    },
+    cover() {
+      return this.trackInfo.album ? this.trackInfo.album.images[0].url : ''
+    },
+    volumeValue() {
+      return Number.parseInt(this.$store.state.Playing.volume * 100)
+    },
+    dislike() {
+      return this.$store.state.Playing.dislike
+    },
+  },
+  methods: {
+    turnUp() {
+      this.$store.commit('turnUp')
+    },
+    turnDown() {
+      this.$store.commit('turnDown')
+    },
+    increaseDislike() {
+      this.isVoted = true
+      this.$store.dispatch('increaseDislike')
+    },
+    reduceDislike() {
+      this.isVoted = false
+      this.$store.commit('reduceDislike')
+    },
+  },
+}
+</script>
+<style lang="scss">
+.sidebar-container {
+  .cover {
+    font-size: 0;
+    img {
+      width: 100%;
+    }
+  }
+
+  .artists,
+  .album {
+    display: flex;
+    justify-content: center;
+  }
+
+  .volumn-control {
+    display: flex;
+    align-items: center;
+    justify-content: space-evenly;
+    .buttons {
+      flex: 0 0 auto;
+      height: 30px;
+      display: flex;
+      align-items: center;
+    }
+    button {
+      height: 25px;
+      width: 25px;
+    }
+    button + button {
+      margin-left: 5px;
+    }
+  }
+  .progress {
+    flex-basis: 120px;
+    font-size: 0;
+  }
+  .bar {
+    height: 6px;
+    padding: 3px;
+    background-color: lightslategray;
+    p {
+      height: 100%;
+      background-color: orange;
+      max-width: 100%;
+      transition: width 0.3 ease-in-out;
+    }
+  }
+
+  .terminate-control {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    .sign {
+      font-size: 0;
+      display: inline-block;
+      padding: 4px;
+      background-color: lightslategray;
+      margin-right: 5px;
+      span {
+        display: inline-block;
+        height: 20px;
+        width: 35px;
+      }
+      span + span {
+        margin-left: 4px;
+      }
+    }
+    .buttons {
+      margin-left: auto;
+    }
+    button + button {
+      margin-left: 5px;
+    }
+  }
+  .active {
+    background-color: orange;
+  }
+  .collect {
+    padding: 10px;
+    button {
+      width: 100%;
+    }
+  }
+}
+</style>
