b6d2029719f562e44adfcd133db914cd7160a4f6
G100my <G100my.lan229@gmail.com>
2021-02-04 22:55:10 +0800

    feat: router.beforeEach - remove hash from spotify redirect

--- src/main.js
+++ src/main.js
@@ -4,6 +4,18 @@ import router from './router'
 import store from './store/store.js'
 import spotifyAPI from './plugin/spotify-web-api.js'
 
+router.beforeEach(to => {
+  if (to.fullPath.includes('access_token')) {
+    // in Hash History mode
+    const hash = to.fullPath
+    const expiredTime = Number(hash.slice(hash.search(/(?<=expires_in=)[\w+]/))) + Date.now()
+    const newToken = hash.substring(hash.search(/(?<=access_token=)[\w+]/), hash.indexOf('&token_type'))
+    store.commit('refreshToken', { newToken, expiredTime })
+    const originPath = hash.slice(0, hash.search('access_token='))
+    return { path: originPath }
+  }
+})
+
 // prettier-ignore
 createApp(App)
   .use(router)
