c0071afa32099e1b576b6c9333bbd16fad467f92
G100my <G100my.lan229@gmail.com>
2021-02-06 01:55:27 +0800

    feat: init router config

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,42 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
+import RoomQueue from '../views/RoomQueue.vue'
+import Room from '../views/Room.vue'
+import Doorscope from '../views/Doorscope.vue'
+import SearchResult from '../views/SearchResult.vue'
+import PlaylistContent from '../views/PlaylistContent.vue'
 
-const routes = []
+const routes = [
+  {
+    path: '/',
+    name: 'Doorscope',
+    component: Doorscope,
+  },
+  {
+    path: '/room',
+    name: 'Room',
+    component: Room,
+    meta: { requiresAuth: true },
+    children: [
+      {
+        path: 'playlist-content',
+        name: 'PlaylistContent',
+        component: PlaylistContent,
+      },
+      {
+        path: 'room-queue',
+        name: 'RoomQueue',
+        component: RoomQueue,
+      },
+      {
+        path: 'search-result',
+        name: 'SearchResult',
+        component: SearchResult,
+      },
+    ],
+  },
+]
 
 const router = createRouter({
-  // 因為是沒有 backend server 的 single page application，使用 hash 來處理 router
   history: createWebHashHistory(),
   routes,
 })
---
+++ src/views/SearchResult.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>search result</h1>
+</template>
