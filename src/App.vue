7e0c8a3a71cadc3737f0e4c4eb7889d1cb417313
G100my <G100my.lan229@gmail.com>
2021-02-07 00:03:26 +0800

    feat: defined :root color variable

--- src/App.vue
+++ src/App.vue
@@ -7,18 +7,36 @@
 export default {}
 </script>
 <style lang="scss">
-* {
-  outline: 1px lightblue dashed;
+:root {
+  --primary-dark: #13192c;
+  --secondary-dark: #0c0f1c;
+
+  --primary-light: #f2f3f7;
+  --secondary-light: #ffffff;
+
+  --primary-highlight: #dd6e42;
+  --secondary-highlight: #cf5626;
+
+  --primary-neutral: #4b90c2;
+  --secondary-neutral: #5aa4da;
+
+  --ignore: #c0c0c0;
 }
 body {
   margin: 0;
-  font-size: 18px;
+  font-size: 16px;
+  font-family: 'Roboto', sans-serif;
+  background-color: var(--primary-dark);
 }
 main {
-  height: 100vh;
-  width: 100vw;
+  min-height: 100vh;
+  min-width: 100%;
+  color: var(--primary-light);
+  display: flex;
+  align-items: center;
 }
-h1 {
+h1,
+h2 {
   margin: 0;
 }
 p {
@@ -26,8 +44,12 @@ p {
 }
 a {
   text-decoration: none;
+  text-decoration-color: var(--primary-neutral);
+  &:visited {
+    color: var(--ignore);
+  }
   &:hover {
-    filter: invert(0.5);
+    color: var(--primary-neutral);
   }
 }
 ul {
