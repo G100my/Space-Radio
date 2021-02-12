43453f8a4d7eda15bd83e9bb7a157d4286d1f337
G100my <G100my.lan229@gmail.com>
2021-02-12 23:10:45 +0800

    style: code indent

--- src/components/TheSearchBar.vue
+++ src/components/TheSearchBar.vue
@@ -26,39 +26,39 @@
         <p>No result</p>
       </template>
       <template v-else>
-      <ul>
-        <li v-for="track in tracksResult" :key="track.id">
-          <div class="result-track">
-            <img :src="track.album.images[track.album.images.length - 1].url" alt="" />
-            <div class="track-info">
-              <p>{{ track.name }}</p>
-              <p>
-                <span v-for="(artist, index) in track.artists" :key="index">{{ artist.name }}</span>
-              </p>
+        <ul>
+          <li v-for="track in tracksResult" :key="track.id">
+            <div class="result-track">
+              <img :src="track.album.images[track.album.images.length - 1].url" alt="" />
+              <div class="track-info">
+                <p>{{ track.name }}</p>
+                <p>
+                  <span v-for="(artist, index) in track.artists" :key="index">{{ artist.name }}</span>
+                </p>
+              </div>
+              <div class="buttons">
+                <button @click="addHandler(track.id)">
+                  <!-- prettier-ignore -->
+                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16">
+                    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+                    <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
+                  </svg>
+                </button>
+                <button @click="jumpInHandler(track.id)">
+                  <!-- prettier-ignore -->
+                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-up-circle" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-7.5 3.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707V11.5z"/>
+                  </svg>
+                </button>
+              </div>
             </div>
-            <div class="buttons">
-              <button @click="addHandler(track.id)">
-                <!-- prettier-ignore -->
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16">
-                  <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
-                  <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
-                </svg>
-              </button>
-              <button @click="jumpInHandler(track.id)">
-                <!-- prettier-ignore -->
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-up-circle" viewBox="0 0 16 16">
-                  <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-7.5 3.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707V11.5z"/>
-                </svg>
-              </button>
-            </div>
-          </div>
-        </li>
-      </ul>
-      <ul>
-        <li v-for="artist in artistsResult" :key="artist.external_ids">
-          <p>{{ artist.name }}</p>
-        </li>
-      </ul>
+          </li>
+        </ul>
+        <ul>
+          <li v-for="artist in artistsResult" :key="artist.external_ids">
+            <p>{{ artist.name }}</p>
+          </li>
+        </ul>
       </template>
     </div>
   </div>
