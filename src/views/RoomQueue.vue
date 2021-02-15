583d1d4c1df92a48c3b9ef10c4ae93f9e578aaee
G100my <G100my.lan229@gmail.com>
2021-02-15 23:35:12 +0800

    add trackName prop in NoteDialog

--- src/views/RoomQueue.vue
+++ src/views/RoomQueue.vue
@@ -28,7 +28,7 @@
               <path fill-rule="evenodd" d="M1.646 2.646a.5.5 0 0 1 .708 0L8 8.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"/>
             </svg>
           </button>
-          <button class="edit-button" type="button" @click="editNote(key)">
+          <button class="edit-button" type="button" @click="editNote(key, detailObject.name)">
             <!-- prettier-ignore -->
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16">
               <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5L13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175l-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"/>
@@ -44,7 +44,7 @@
               <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
             </svg>
           </button>
-          <button class="normal2urgent-button" type="button" @click="normal2urgent(key)">
+          <button class="normal2urgent-button" type="button" @click="normal2urgent(key, detailObject.name)">
             <!-- prettier-ignore -->
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-double-up" viewBox="0 0 16 16">
               <path fill-rule="evenodd" d="M7.646 2.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 3.707 2.354 9.354a.5.5 0 1 1-.708-.708l6-6z"/>
@@ -85,20 +85,20 @@ export default {
     remove(key, level) {
       this.$store.dispatch(`${level}Remove`, key)
     },
-    editNote(key) {
+    editNote(key, trackName) {
       const submitFunction = newNote => {
         this.$store.dispatch(`urgentEdit`, { key, note: newNote })
       }
-      this.$emit('activeNoteDialog', key, 'urgent', submitFunction)
+      this.$emit('activeNoteDialog', key, 'urgent', trackName, submitFunction)
     },
     urgent2normal(key) {
       this.$store.dispatch('urgent2normal', key)
     },
-    normal2urgent(key) {
+    normal2urgent(key, trackName) {
       const submitFunction = newNote => {
         this.$store.dispatch('normal2urgent', { key, note: newNote })
       }
-      this.$emit('activeNoteDialog', key, 'normal', submitFunction)
+      this.$emit('activeNoteDialog', key, 'normal', trackName, submitFunction)
     },
   },
 }
