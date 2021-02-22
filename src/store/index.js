377596e4330e8391c335c8cc3494d645868d5359
G100my <G100my.lan229@gmail.com>
2021-02-22 14:20:35 +0800

    init UserLog

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,15 @@
 import { createStore, createLogger } from 'vuex'
 import { PlayingState, playingStateFirebasePlugin } from './PlayingState.js'
 import { Personal } from './Personal.js'
+import { UserLog, userLogFirebasePlugin } from './UserLog.js'
 
 const store = createStore({
   modules: {
     Personal,
     PlayingState,
+    UserLog,
   },
-  plugins: [createLogger(), playingStateFirebasePlugin],
+  plugins: [createLogger(), playingStateFirebasePlugin, userLogFirebasePlugin],
 })
 
 export default store
