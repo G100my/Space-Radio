10e73761ab6c775e69ce68a2d899f073ba5a65eb
G100my <G100my.lan229@gmail.com>
2021-02-23 16:18:02 +0800

    feat: add isPlaying state

--- src/components/host/HostControl.vue
+++ src/components/host/HostControl.vue
@@ -1,7 +1,28 @@
 <template>
   <div class="host-control">
     <button type="button" @click="!deviceActived && activeThisDevice($event), deviceActived && togglePlay($event)">
-      activeThisDevice
+      <svg
+        v-show="paused"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="currentColor"
+        class="bi bi-play-fill"
+        viewBox="0 0 16 16"
+      >
+        <path
+          d="M11.596 8.697l-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393z"
+        />
+      </svg>
+      <svg
+        v-show="!paused"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="currentColor"
+        class="bi bi-pause-fill"
+        viewBox="0 0 16 16"
+      >
+        <path
+          d="M5.5 3.5A1.5 1.5 0 0 1 7 5v6a1.5 1.5 0 0 1-3 0V5a1.5 1.5 0 0 1 1.5-1.5zm5 0A1.5 1.5 0 0 1 12 5v6a1.5 1.5 0 0 1-3 0V5a1.5 1.5 0 0 1 1.5-1.5z"
+        />
+      </svg>
     </button>
 
     <input
@@ -34,6 +55,7 @@ export default {
       dislikeThreshold: 2,
       dislikeCountDownTimer: null,
       deviceActived: false,
+      paused: true,
     }
   },
   computed: {
@@ -124,9 +146,12 @@ export default {
         // 斷開連結
         if (playerState === null) {
           this.deviceActived = false
+          this.paused = true
           this.$store.dispatch('clearPlayingTrack')
           return
         }
+
+        this.paused = playerState.paused
 
         const currentNoteId = playerState.track_window.current_track.id
 
