73f31e6bfc71d6ee28836e6752ad84a9d26eec56
G100my <G100my.lan229@gmail.com>
2021-02-23 15:20:38 +0800

    style: volumn-control - volumn icon

--- src/components/PlayingState.vue
+++ src/components/PlayingState.vue
@@ -24,7 +24,11 @@
     </div>
     <div class="control-board">
       <div class="volumn-control">
-        <img src="../assets/volume.svg" alt="" />
+        <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-volume-down" viewBox="0 0 16 16">
+          <path
+            d="M9 4a.5.5 0 0 0-.812-.39L5.825 5.5H3.5A.5.5 0 0 0 3 6v4a.5.5 0 0 0 .5.5h2.325l2.363 1.89A.5.5 0 0 0 9 12V4zM6.312 6.39L8 5.04v5.92L6.312 9.61A.5.5 0 0 0 6 9.5H4v-3h2a.5.5 0 0 0 .312-.11zM12.025 8a4.486 4.486 0 0 1-1.318 3.182L10 10.475A3.489 3.489 0 0 0 11.025 8 3.49 3.49 0 0 0 10 5.525l.707-.707A4.486 4.486 0 0 1 12.025 8z"
+          />
+        </svg>
         <div class="progress">
           <div class="bar">
             <p :style="{ width: `${currentVolume}%` }" />
@@ -153,6 +157,10 @@ export default {
     display: flex;
     align-items: center;
     justify-content: space-between;
+    svg {
+      height: 30px;
+      width: 30px;
+    }
     .buttons {
       flex: 0 0 auto;
       height: 30px;
