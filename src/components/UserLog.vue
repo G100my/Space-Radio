3e6f61987043d99234591f8e0e287b7e0d2b2c65
G100my <G100my.lan229@gmail.com>
2021-02-23 14:39:19 +0800

    init UserLog

---
+++ src/components/UserLog.vue
@@ -0,0 +1,56 @@
+<template>
+  <ul class="log-container">
+    <li v-for="log in logs" :key="log.timestemp">
+      <p v-if="log.actionType === 'add' || log.actionType === 'jumpIn'">
+        <span>{{ log.userId }}</span
+        >{{ log.actionType === 'add' ? '點' : '插' }}播了<span>{{ log.option.trackName }}</span>
+      </p>
+      <p v-else-if="log.actionType === 'normalRemove' || log.actionType === 'urgentRemove'">
+        <span>{{ log.userId }}</span
+        >從{{ log.actionType === 'normalRemove' ? '點' : '插' }}播序列移除了<span>{{ log.option.trackName }}</span>
+      </p>
+      <p v-else-if="log.actionType === 'normal2urgent' || log.actionType === 'urgent2normal'">
+        <span>{{ log.userId }}</span
+        >把<span>{{ log.option.trackName }}</span
+        >移到{{ log.actionType === 'normal2urgent' ? '點' : '插' }}播序列
+      </p>
+      <p v-else-if="log.actionType === 'turnUp' || log.actionType === 'turnDown'">
+        <span>{{ log.userId }}</span
+        >調高音量至:<span>{{ log.option.volume }}</span>
+      </p>
+    </li>
+  </ul>
+</template>
+<script>
+export default {
+  computed: {
+    logs() {
+      return this.$store.getters.userLog
+    },
+  },
+}
+</script>
+<style lang="scss">
+.log-container {
+  height: 100%;
+  padding: 10px 0;
+  border: 1px solid var(--primary-highlight);
+  background-color: var(--secondary-dark);
+  border-radius: var(--border-radius);
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  p {
+    text-align: center;
+    color: var(--ignore);
+  }
+  span {
+    display: inline-block;
+    margin: 0 5px;
+    color: var(--secondary-neutral);
+    &:first-child {
+      color: var(--secondary-highlight);
+    }
+  }
+}
+</style>
