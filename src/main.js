aa712c82985091b841f5200bee106dbc7f8ae2f9
G100my <G100my.lan229@gmail.com>
2021-02-24 15:35:16 +0800

    feat: move router.beforeEach from main to router/index

--- src/main.js
+++ src/main.js
@@ -2,28 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store/'
-import spotifyPlugin, { spotifyAPI } from './plugin/spotify-web-api.js'
-
-router.beforeEach(to => {
-  if (to.fullPath.includes('access_token')) {
-    const hash = to.fullPath
-    const expires_in = Number(hash.slice(hash.search(/(?<=expires_in=)[\w+]/))) * 1000
-    const expiredTime = expires_in + Date.now()
-
-    const newToken = hash.substring(hash.search(/(?<=access_token=)[\w+]/), hash.indexOf('&token_type'))
-    store.commit('refreshToken', { newToken, expiredTime })
-    spotifyAPI.setAccessToken(newToken)
-    return { name: 'Room' }
-  }
-
-  if (!to.meta.requiresAuth && store.getters.isTokenValid) {
-    return { name: 'Room' }
-  }
-
-  if (to.meta.requiresAuth && !store.getters.isTokenValid) {
-    return { name: 'Doorscope' }
-  }
-})
+import spotifyPlugin from './plugin/spotify-web-api.js'
 
 // prettier-ignore
 createApp(App)
--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,6 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
+import { spotifyAPI } from '../plugin/spotify-web-api.js'
+import store from '../store/'
 import RoomQueue from '../views/RoomQueue.vue'
 import Room from '../views/Room.vue'
 import Doorscope from '../views/Doorscope.vue'
@@ -27,6 +29,27 @@ const routes = [
 const router = createRouter({
   history: createWebHashHistory(),
   routes,
+})
+
+router.beforeEach(to => {
+  if (to.fullPath.includes('access_token')) {
+    const hash = to.fullPath
+    const expires_in = Number(hash.slice(hash.search(/(?<=expires_in=)[\w+]/))) * 1000
+    const expiredTime = expires_in + Date.now()
+
+    const newToken = hash.substring(hash.search(/(?<=access_token=)[\w+]/), hash.indexOf('&token_type'))
+    store.commit('refreshToken', { newToken, expiredTime })
+    spotifyAPI.setAccessToken(newToken)
+    return { name: 'Room' }
+  }
+
+  if (!to.meta.requiresAuth && store.getters.isTokenValid) {
+    return { name: 'Room' }
+  }
+
+  if (to.meta.requiresAuth && !store.getters.isTokenValid) {
+    return { name: 'Doorscope' }
+  }
 })
 
 export default router
