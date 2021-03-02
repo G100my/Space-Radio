93f4aebabd1e4cb0b6fa58b776a6a77823b138a8
G100my <G100my.lan229@gmail.com>
2021-03-02 15:50:13 +0800

    refactor: split NavAdditionDisplay as component

---
+++ src/components/NavAdditionDisplay.vue
@@ -0,0 +1,106 @@
+<template>
+  <div class="immediately-result">
+    <template v-if="source.length === 0">
+      <p class="no-result" @click="$emit('activeSearchStyle', false)">No result</p>
+    </template>
+    <template v-else>
+      <TrackGridShell>
+        <template #body>
+          <TrackGridItem v-for="track in source" :key="track.id" :info="track">
+            <AddButton :track-id="track.id" :track-name="track.name" />
+            <JumpInButton :track-id="track.id" :track-name="track.name" />
+          </TrackGridItem>
+        </template>
+      </TrackGridShell>
+    </template>
+  </div>
+  <div class="result-mask" :class="{ active }" />
+</template>
+<script>
+import TrackGridItem from './template/TrackGridItem.vue'
+import TrackGridShell from './template/TrackGridShell.vue'
+import AddButton from './featureButtons/AddButton.vue'
+import JumpInButton from './featureButtons/JumpInButton.vue'
+
+export default {
+  components: {
+    AddButton,
+    JumpInButton,
+    TrackGridItem,
+    TrackGridShell,
+  },
+  props: {
+    source: {
+      type: Array,
+      default: () => [],
+    },
+  },
+  emits: ['activeSearchStyle'],
+  created() {
+    console.log(this.$attrs)
+  },
+}
+</script>
+<style lang="scss">
+.no-result {
+  height: 100%;
+  width: 100%;
+  text-align: center;
+  margin: 0;
+  overflow: hidden;
+}
+
+.immediately-result {
+  transition-duration: 0.3s;
+  transition-timing-function: ease-in-out;
+  transition-property: width, height;
+  box-sizing: border-box;
+  position: absolute;
+  top: 100%;
+  left: 0;
+  right: 0;
+  height: 0;
+  width: 90vw;
+  z-index: -1;
+  overflow-y: auto;
+  margin: 0 auto;
+  @media (min-width: 768px) {
+    margin: 0 0 0 auto;
+    width: 0;
+  }
+}
+
+.result-mask {
+  transition-duration: 0.3s;
+  transition-timing-function: ease-in-out;
+  transition-property: width, height;
+  box-sizing: border-box;
+  position: absolute;
+  right: 0;
+  height: 0;
+  background-color: var(--primary-dark);
+  top: 0;
+  width: 100vw;
+  z-index: -2;
+  opacity: 0.9;
+  @media (min-width: 768px) {
+    height: 100vh;
+    width: 0;
+  }
+}
+.active-search {
+  &.result-mask {
+    height: 100vh;
+    @media (min-width: 768px) {
+      width: 50vw;
+    }
+  }
+  & .immediately-result {
+    height: calc(100vh - 70px - 30px);
+    @media (min-width: 768px) {
+      width: 50vw;
+      padding: 15px;
+    }
+  }
+}
+</style>
