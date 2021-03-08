5988623e8293d5cd18ce63acfd5c105d651eb565
G100my <G100my.lan229@gmail.com>
2021-03-09 01:49:03 +0800

    feat: vite.config - base depend on command

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,16 @@ import vue from '@vitejs/plugin-vue'
 /**
  * @type {import('vite').UserConfig}
  */
-export default {
-  plugins: [vue()],
-  optimizeDeps: {
-    include: ['firebase/app', 'firebase/database', 'firebase/analytics'],
-    exclude: ['firebase'],
-  },
+export default ({ command }) => {
+  return {
+    plugins: [vue()],
+    optimizeDeps: {
+      include: ['firebase/app', 'firebase/database', 'firebase/analytics'],
+      exclude: ['firebase'],
+    },
+    base: command === 'build' ? '/Jukebox/' : './',
+    build: {
+      outDir: './docs/',
+    },
+  }
 }
