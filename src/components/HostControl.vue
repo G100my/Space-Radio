7e3642c506fedd794a79efc82897017a8688fcee
G100my <G100my.lan229@gmail.com>
2021-04-04 23:58:04 +0800

    refactor: HostControl - composition API

--- src/components/HostControl.vue
+++ src/components/HostControl.vue
@@ -1,10 +1,6 @@
 <template>
   <div class="main-control">
-    <button
-      class="play-button"
-      type="button"
-      @click="!deviceActived && player.connect(), deviceActived && togglePlay($event)"
-    >
+    <button class="play-button" type="button" @click="togglePlay">
       <svg
         v-show="paused"
         xmlns="http://www.w3.org/2000/svg"
@@ -60,108 +56,56 @@
   </div>
 </template>
 <script>
-import { mapGetters } from 'vuex'
+import { ref, watch, computed } from 'vue'
+import { useStore } from 'vuex'
+import { spotifyAPI as $spotifyAPI } from '../plugin/spotify-web-api.js'
 import { refreshAccessToken } from '../utility/PKCE.js'
 import { messageOutputMaker } from '../utility/messageOutputMaker.js'
 
 export default {
-  data() {
-    return {
-      player: null,
-      deviceId: null,
-      playerVolume: 50,
-      deviceActived: false,
-      paused: true,
+  setup() {
+    const store = useStore()
 
-      positionStateCounter: 0,
-      coundDownTimer: null,
+    let dislikeThreshold = 2
+    let minimalVolume = 50
+    const isShowMinimalControlBoard = ref(false)
 
-      utterance: new window.SpeechSynthesisUtterance(),
-      recodeVolume: null,
-      executeBeforeEndTime: 10000,
-      adjustProcessTime: 5000,
-      adjustStepTime: 100,
+    const playerPlayingTrackId = computed(() => store.getters.playerPlayingTrackId)
+    const currentVolume = computed(() => store.getters.currentVolume)
+    const currentMinimalVolume = computed(() => store.getters.currentMinimalVolume)
+    const currentDislike = computed(() => store.getters.currentDislike)
+    const currentDislikeThreshold = computed(() => store.getters.currentDislikeThreshold)
+    const leftQueueAmount = computed(() => store.getters.leftQueueAmount)
+    const token = computed(() => store.getters.token)
+    const isTokenValid = computed(() => store.getters.isTokenValid)
 
-      dislikeThreshold: 2,
-      dislikeCountdownTimer: null,
-      minimalVolume: 50,
-      minimalVolumeDeferTimer: null,
+    const dislikeThresholdInput = ref(null)
+    const minimalVolumeInput = ref(null)
 
-      isShowMinimalControlBoard: false,
-    }
-  },
-  computed: {
-    adjustExecuteTimes() {
-      return this.adjustProcessTime / this.adjustStepTime
-    },
-    ...mapGetters([
-      'playerPlayingTrackId',
-      'currentVolume',
-      'currentMinimalVolume',
-      'currentDislike',
-      'currentDislikeThreshold',
-      'trackData',
-      'pendingQueue',
-      'leftQueueAmount',
-      'token',
-      'isTokenValid',
-    ]),
-  },
-  watch: {
-    pendingQueue(nextQueue) {
-      if (nextQueue && nextQueue.note) {
-        const note = nextQueue.note
-        let messageOutput4TTS = messageOutputMaker(note, nextQueue.track_name)
-        messageOutput4TTS = messageOutput4TTS.replace(/[^\w^\s^\u4e00-\u9fa5]/gi, '')
-        this.$store.dispatch('updateTheLatestQueue', nextQueue)
+    let player
+    let playerVolume = 50
+    let positionStateCounter = 0
+    let recodeVolume
+    const executeBeforeEndTime = 10000
+    let adjustProcessTime = 5000
+    let adjustStepTime = 100
+    let deviceActived = false
+    let deviceId
+    let coundDownTimer
+    let dislikeCountdownTimer
+    const paused = ref(true)
+    const adjustExecuteTimes = computed(() => adjustProcessTime / adjustStepTime)
 
-        this.reducePlayerVolume()
-          .then(() => {
-            this.TTS(messageOutput4TTS)
-          })
-          .catch(error => {
-            console.error(error)
-          })
-      }
-    },
-    currentDislike(newValue) {
-      if (this.dislikeCountdownTimer && newValue < this.dislikeThreshold) {
-        clearTimeout(this.dislikeCountdownTimer)
-        this.dislikeCountdownTimer = null
-      }
-      if (newValue >= this.dislikeThreshold) {
-        let counter = 10
-        this.dislikeCountdownTimer = setInterval(() => {
-          counter -= 1
-          this.$store.dispatch('updateDislikeCountdown', counter)
-          console.log(counter)
-          if (counter <= 0) {
-            this.nextTrack()
-            clearInterval(this.dislikeCountdownTimer)
-            this.$store.dispatch('clearDislikeVote')
-            this.$store.dispatch('updateDislikeCountdown', false)
-            this.dislikeCountdownTimer = null
-          }
-        }, 1000)
-      }
-    },
-  },
-  created() {
-    window.onbeforeunload = () => {
-      this.$store.dispatch('clearPlayingTrack')
-      this.$store.dispatch('clearPendingQueue')
-      if (this.deviceActived) this.player.disconnect()
-    }
     window.onSpotifyWebPlaybackSDKReady = () => {
-      this.player = new window.Spotify.Player({
+      player = new window.Spotify.Player({
         name: 'Jukebox player',
-        volume: this.currentVolume / 100,
+        volume: currentVolume.value / 100,
         getOAuthToken: callback => {
-          if (this.isTokenValid) {
-            callback(this.token)
+          if (isTokenValid.value) {
+            callback(token.value)
           } else {
             refreshAccessToken().then(() => {
-              callback(this.token)
+              callback(token.value)
             })
           }
         },
@@ -174,210 +118,273 @@ export default {
         'not_ready',
       ]
       eventArray.forEach(event => {
-        this.player.addListener(event, message => {
+        player.addListener(event, message => {
           console.log(event, message)
         })
       })
 
       // Ready
-      this.player.addListener('ready', ({ device_id }) => {
+      player.addListener('ready', ({ device_id }) => {
         console.log('Ready with Device ID', device_id)
-        this.deviceId = device_id
+        deviceId = device_id
         // 把目前 host 帳號可能在其他地方播放的音樂轉移到 player，並且直接撥放
-        this.$spotifyAPI.transferMyPlayback([this.deviceId], { play: true }, error => {
+        $spotifyAPI.transferMyPlayback([deviceId], { play: false }, error => {
           error && console.log(error.response)
           if (!error) {
-            this.deviceActived = true
+            deviceActived = true
           }
         })
         // 等 player 準備完成才 watch playerVolume
-        this.$watch('currentVolume', newValue => {
+        watch(currentVolume, newValue => {
           console.log('currentVolume')
-          this.playerVolume = newValue
-          console.log(this.player)
-          if (this.player !== null) this.player.setVolume(newValue / 100)
+          playerVolume = newValue
+          console.log(player)
+          if (player !== null) player.setVolume(newValue / 100)
         })
       })
 
       // 避免中途重啟 pending 會一直常駐，直到下一首歌曲取代目前的 pending
       const checkPending = state => {
-        if (this.pendingQueue) {
-          if (state.track_window.next_tracks[0].id !== this.pendingQueue.id) this.$store.dispatch('clearPendingQueue')
+        if (pendingQueue.value) {
+          if (state.track_window.next_tracks[0].id !== pendingQueue.value.id) store.dispatch('clearPendingQueue')
         }
-        this.player.removeListener('player_state_changed', checkPending)
+        player.removeListener('player_state_changed', checkPending)
       }
-      this.player.addListener('player_state_changed', checkPending)
+      player.addListener('player_state_changed', checkPending)
 
       // Playback status updates
-      this.player.addListener('player_state_changed', playerState => {
+      player.addListener('player_state_changed', playerState => {
         console.log(playerState)
         // 斷開連結
         if (playerState === null) {
-          this.deviceActived = false
-          this.paused = true
-          this.$store.dispatch('clearPlayingTrack')
+          deviceActived = false
+          paused.value = true
+          store.dispatch('clearPlayingTrack')
           return
         }
 
-        this.paused = playerState.paused
+        paused.value = playerState.paused
 
         const currentNoteId = playerState.track_window.current_track.id
 
         // 更新 playingState, 如果 playingState 的 track id 和 player 回傳的 id 不一樣
-        if (currentNoteId !== this.playerPlayingTrackId) {
+        if (currentNoteId !== playerPlayingTrackId.value) {
           const playingTrack = playerState.track_window.current_track
-          this.$store.dispatch('updatePlayingTrack', playingTrack)
+          store.dispatch('updatePlayingTrack', playingTrack)
         }
 
         if (playerState.position === 0) {
-          this.positionStateCounter++
-          if (this.positionStateCounter >= 2) {
-            this.positionStateCounter = 0
+          positionStateCounter++
+          if (positionStateCounter >= 2) {
+            positionStateCounter = 0
 
             // 如果已經有 pending queue 而且跟現在正在撥放的是同一首歌，清空 pending
-            if (this.pendingQueue && this.pendingQueue.id === currentNoteId) {
-              this.$store.dispatch('clearPendingQueue')
+            if (pendingQueue.value && pendingQueue.value.id === currentNoteId) {
+              store.dispatch('clearPendingQueue')
             }
           }
         }
 
-        if (!playerState.paused && this.leftQueueAmount > 0 && !this.pendingQueue) {
+        if (!playerState.paused && leftQueueAmount.value > 0 && !pendingQueue.value) {
           // 防止開啟多個頁面且登入同樣的 host account 還都執行撥放，造成短時間內重複 dispatch sendNextQueue
           const randomTime = Math.floor(Math.random() * 5) * 5 * 1000
-          const bufferTime = playerState.duration - playerState.position - this.executeBeforeEndTime - randomTime
+          const bufferTime = playerState.duration - playerState.position - executeBeforeEndTime - randomTime
           // 目前歌曲結束前幾秒(executeBeforeEndTime)插入新的歌，如果被快轉至小於 executeBeforeEndTime 的剩餘時間就不插入
           if (bufferTime > 0) {
             // 每次隨機狀態出現就刷新秒數，避免曲目被快轉
-            if (this.coundDownTimer) clearTimeout(this.coundDownTimer)
+            if (coundDownTimer) clearTimeout(coundDownTimer)
             console.log('set coundDownTimer')
-            this.coundDownTimer = setTimeout(() => {
-              this.$store.dispatch('sendNextQueue')
+            coundDownTimer = setTimeout(() => {
+              store.dispatch('sendNextQueue')
             }, bufferTime)
           }
         }
       })
+
+      player.connect()
+    }
+    window.onbeforeunload = () => {
+      store.dispatch('clearPlayingTrack')
+      store.dispatch('clearPendingQueue')
+      if (deviceActived.value) player.disconnect()
     }
 
     import('../utility/spotify-player-SDK.js')
 
-    this.utterance.pitch = 1
-    this.utterance.rate = 1
-    this.utterance.volume = 1
-    this.utterance.lang = 'zh-TW'
-    this.utterance.onerror = error => {
-      console.log('utterance error', error)
-      this.resumePlayerVolume()
+    const utterance = new window.SpeechSynthesisUtterance()
+    utterance.pitch = 1
+    utterance.rate = 1
+    utterance.volume = 1
+    utterance.lang = 'zh-TW'
+    utterance.onerror = error => {
+      console.log('utterance error: ', error)
+      resumePlayerVolume()
     }
-    this.utterance.onend = () => {
+    utterance.onend = () => {
       console.log('utterance end')
-      this.resumePlayerVolume()
+      resumePlayerVolume()
     }
+
     speechSynthesis.onvoiceschanged = () => {
-      if (!this.utterance.voice) this.setTTSVoice()
+      if (!utterance.voice) setTTSVoice()
     }
-  },
-  methods: {
-    setTTSVoice() {
+
+    const pendingQueue = computed(() => store.getters.pendingQueue)
+
+    watch(pendingQueue, nextQueue => {
+      if (nextQueue && nextQueue.note) {
+        const note = nextQueue.note
+        let messageOutput4TTS = messageOutputMaker(note, nextQueue.track_name)
+        messageOutput4TTS = messageOutput4TTS.replace(/[^\w^\s^\u4e00-\u9fa5]/gi, '')
+        store.dispatch('updateTheLatestQueue', nextQueue)
+
+        reducePlayerVolume()
+          .then(() => {
+            TTS(messageOutput4TTS)
+          })
+          .catch(error => {
+            console.error(error)
+          })
+      }
+    })
+
+    watch(currentDislike, newValue => {
+      if (dislikeCountdownTimer && newValue < dislikeThreshold) {
+        clearTimeout(dislikeCountdownTimer)
+        dislikeCountdownTimer = null
+      }
+      if (newValue >= dislikeThreshold) {
+        let counter = 10
+        dislikeCountdownTimer = setInterval(() => {
+          counter -= 1
+          store.dispatch('updateDislikeCountdown', counter)
+          console.log(counter)
+          if (counter <= 0) {
+            nextTrack()
+            clearInterval(dislikeCountdownTimer)
+            store.dispatch('clearDislikeVote')
+            store.dispatch('updateDislikeCountdown', false)
+            dislikeCountdownTimer = null
+          }
+        }, 1000)
+      }
+    })
+
+    function setTTSVoice() {
       const voice = speechSynthesis
         .getVoices()
         .find(item => item.name.includes('Google') && item.lang.includes('zh-TW'))
-      if (voice !== null) this.utterance.voice = voice
-    },
-    TTS(text) {
-      if (this.utterance.voice === null) this.setTTSVoice()
-      this.utterance.text = text
-      speechSynthesis.speak(this.utterance)
-    },
-    reducePlayerVolume() {
+      if (voice !== null) utterance.voice = voice
+    }
+    function TTS(text) {
+      if (utterance.voice === null) setTTSVoice()
+      utterance.text = text
+      speechSynthesis.speak(utterance)
+    }
+    function reducePlayerVolume() {
       return new Promise(success => {
-        this.recodeVolume = this.playerVolume
+        recodeVolume = playerVolume
 
-        const step = (this.playerVolume - this.currentMinimalVolume) / this.adjustExecuteTimes
+        const step = (playerVolume - currentMinimalVolume.value) / adjustExecuteTimes.value
 
         const timer = setInterval(() => {
-          const afterStep = this.playerVolume - step
-          this.player.setVolume(afterStep / 100)
-          if (afterStep < this.currentMinimalVolume) {
+          const afterStep = playerVolume - step
+          player.setVolume(afterStep / 100)
+          if (afterStep < currentMinimalVolume.value) {
             clearInterval(timer)
             success()
             return
           }
-          this.playerVolume = afterStep
-        }, this.adjustStepTime)
+          playerVolume = afterStep
+        }, adjustStepTime)
       })
-    },
-    resumePlayerVolume() {
+    }
+    function resumePlayerVolume() {
       return new Promise(success => {
-        const step = (this.recodeVolume - this.playerVolume) / this.adjustExecuteTimes
+        const step = (recodeVolume - playerVolume) / adjustExecuteTimes.value
 
         const timer = setInterval(() => {
-          const afterStep = this.playerVolume + step
-          this.player.setVolume(afterStep / 100)
-          if (afterStep > this.recodeVolume) {
+          const afterStep = playerVolume + step
+          player.setVolume(afterStep / 100)
+          if (afterStep > recodeVolume) {
             clearInterval(timer)
-            this.playerVolume = this.recodeVolume
-            this.recodeVolume = null
+            playerVolume = recodeVolume
+            recodeVolume = null
             success()
             return
           }
-          this.playerVolume = afterStep
-        }, this.adjustStepTime)
+          playerVolume = afterStep
+        }, adjustStepTime)
       })
-    },
-    nextTrack() {
-      const minimalVolumeBackup = this.minimalVolume
-      const adjustProcessTimeBackup = this.adjustProcessTime
-      this.minimalVolume = 0
-      this.adjustProcessTime = 3000
+    }
+    function nextTrack() {
+      const minimalVolumeBackup = minimalVolume
+      const adjustProcessTimeBackup = adjustProcessTime
+      minimalVolume = 0
+      adjustProcessTime = 3000
 
       let counter = 0
       const secondPositionStateHandler = ({ position }) => {
         if (position === 0) counter++
         // 觀察 state 行為，第二次 position == 0 的 state 發生後才會撥放
         if (counter >= 2) {
-          this.player.removeListener('player_state_changed', secondPositionStateHandler)
-          this.resumePlayerVolume()
+          player.removeListener('player_state_changed', secondPositionStateHandler)
+          resumePlayerVolume()
             .then(() => {
-              this.minimalVolume = minimalVolumeBackup
-              this.adjustProcessTime = adjustProcessTimeBackup
+              minimalVolume = minimalVolumeBackup
+              adjustProcessTime = adjustProcessTimeBackup
             })
             .catch(error => console.error(error))
         }
       }
 
-      this.reducePlayerVolume().then(() => {
-        this.$store.dispatch('sendNextQueue', () => {
+      reducePlayerVolume().then(() => {
+        store.dispatch('sendNextQueue', () => {
           // 神秘的 reason 參數，並沒有出現在文件，
           // 但是不給會有 error: parameter 'reason' is required
-          this.player
+          player
             .nextTrack('just wanna listen next one')
             .then(() => {
               console.log('Skipped to next track!')
-              this.player.addListener('player_state_changed', secondPositionStateHandler)
+              player.addListener('player_state_changed', secondPositionStateHandler)
             })
             .catch(error => console.error(error))
         })
       })
-    },
-    togglePlay() {
-      this.player.togglePlay(this.deviceId).then(() => console.log('toggle play'))
-    },
-    submitHandler() {
-      const editedMinimalVolume = Number.parseInt(this.$refs.minimalVolumeInput.value)
-      if (this.minimalVolume !== editedMinimalVolume) {
-        this.minimalVolume = editedMinimalVolume
-        this.$store.dispatch('updateMinimalVolume', this.minimalVolume)
+    }
+    function togglePlay() {
+      player.togglePlay(deviceId).then(() => console.log('toggle play'))
+    }
+    function submitHandler() {
+      const editedMinimalVolume = Number.parseInt(minimalVolumeInput.value.value)
+      if (minimalVolume !== editedMinimalVolume) {
+        minimalVolume = editedMinimalVolume
+        store.dispatch('updateMinimalVolume', minimalVolume)
       }
-      const editedDislikeThreshold = Number.parseInt(this.$refs.dislikeThresholdInput.value)
-      if (this.dislikeThreshold !== editedDislikeThreshold) {
-        this.dislikeThreshold = editedDislikeThreshold
-        this.$store.dispatch('updateDislikeThreshold', this.dislikeThreshold)
+      const editedDislikeThreshold = Number.parseInt(dislikeThresholdInput.value.value)
+      if (dislikeThreshold !== editedDislikeThreshold) {
+        dislikeThreshold = editedDislikeThreshold
+        store.dispatch('updateDislikeThreshold', dislikeThreshold)
       }
-    },
-    resetHandler() {
-      this.$refs.minimalVolumeInput.value = this.minimalVolume
-      this.$refs.dislikeThresholdInput.value = this.dislikeThreshold
-    },
+    }
+    function resetHandler() {
+      minimalVolumeInput.value = minimalVolume
+      dislikeThresholdInput.value = dislikeThreshold
+    }
+
+    return {
+      deviceActived,
+      player,
+      togglePlay,
+      paused,
+      isShowMinimalControlBoard,
+      currentMinimalVolume,
+      currentDislikeThreshold,
+      submitHandler,
+      resetHandler,
+      minimalVolumeInput,
+      dislikeThresholdInput,
+    }
   },
 }
 </script>
