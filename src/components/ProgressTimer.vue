3cef54164f341d68081c058ddd2b332796313a07
G100my <G100my.lan229@gmail.com>
2021-04-08 02:14:59 +0800

    feat: init ProgressTimer

---
+++ src/components/ProgressTimer.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="progress-timer">
+    <p>
+      <span>{{ positionMin }}:{{ positionSec }}</span
+      ><span class="midline">/</span><span>{{ durationMin }}:{{ durationSec }}</span>
+    </p>
+  </div>
+</template>
+<script>
+import { watch, computed, ref } from 'vue'
+import { useStore } from 'vuex'
+
+export default {
+  setup() {
+    const interval = 1000
+    let lastTimeStamp = 0
+
+    const store = useStore()
+    const currentProgress = computed(() => store.getters.currentProgress)
+
+    const progressTimer = ref(null)
+    const durationMin = ref('-')
+    const durationSec = ref('--')
+    const positionMin = ref('-')
+    const positionSec = ref('--')
+
+    function clearTimeDisplay() {
+      positionMin.value = '-'
+      positionSec.value = '--'
+      durationMin.value = '-'
+      durationSec.value = '--'
+    }
+    function zeroFormatter(num) {
+      return num < 10 ? '0'.concat(num) : num
+    }
+    function typeCheck(item) {
+      if (typeof item !== 'number') {
+        console.log(`progress time value is not Number, it's ${typeof item}`)
+        return Number(item)
+      }
+      return item
+    }
+
+    watch(currentProgress, newProgress => {
+      clearInterval(progressTimer.value)
+      let { timestamp, duration, position } = newProgress
+      // FIXME
+      timestamp = typeCheck(timestamp)
+      duration = typeCheck(duration)
+      position = typeCheck(position)
+
+      if (timestamp !== 0 && timestamp - lastTimeStamp > 5000 && duration - position > 2000) {
+        lastTimeStamp = timestamp
+
+        durationMin.value = Math.floor(duration / 1000 / 60)
+        durationSec.value = zeroFormatter(Math.floor((duration / 1000) % 60))
+
+        progressTimer.value = setInterval(() => {
+          position += 1000
+          if (position > duration) {
+            clearInterval(progressTimer.value)
+            clearTimeDisplay()
+          } else {
+            positionMin.value = Math.floor(position / 1000 / 60)
+            positionSec.value = zeroFormatter(Math.floor((position / 1000) % 60))
+          }
+        }, interval)
+      } else if (timestamp === 0) {
+        clearTimeDisplay()
+      }
+    })
+    return {
+      durationMin,
+      durationSec,
+      positionMin,
+      positionSec,
+      currentProgress,
+    }
+  },
+}
+</script>
+<style lang="scss">
+.progress-timer {
+  .midline {
+    margin: 0 0.5em;
+  }
+}
+</style>
