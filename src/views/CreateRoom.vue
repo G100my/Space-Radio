3641cba4c82964ababbbc951fe99ff68b129ac34
G100my <G100my.lan229@gmail.com>
2021-04-19 22:17:21 +0800

    update CreateRoom style

--- src/views/CreateRoom.vue
+++ src/views/CreateRoom.vue
@@ -1,49 +1,42 @@
 <template>
-  <div class="">
-    <h1>Create Room</h1>
-    <form>
-      <p>
-        <label>Host id : </label>
-        <span style="color: var(--ignore)">{{ userId }}</span>
-      </p>
-      <p>
-        <label>Room Key : </label>
-        <span style="color: var(--ignore)">{{ roomKey }}</span>
-      </p>
-      <p class="">
-        <label for="room-name">Room name : </label>
-        <input id="room-name" v-model.trim="roomName" type="text" maxlength="50" autocomplete="off" />
-      </p>
-      <p>
-        <label for="init-volume"
-          >Initail volume : <span>{{ volume }}</span></label
+  <HallShell>
+    <div class="mt-24">
+      <h2 class="text-subtitle font-semibold mb-4">Create room</h2>
+      <form class="form">
+        <p>
+          <label>Host ID : </label>
+          <input :value="userId" />
+        </p>
+        <p>
+          <label>Room Key : </label>
+          <input :value="roomKey" />
+        </p>
+        <p>
+          <label for="room-name">Room name : </label>
+          <input id="room-name" v-model.trim="roomName" type="text" maxlength="50" autocomplete="off" />
+        </p>
+        <button
+          type="button"
+          class="w-full p-2 text-spotify border-2 border-spotify rounded active:border-spotify active:bg-spotify active:text-white"
+          :disabled="hasSameRoomName || roomName.length == 0"
+          @click="createRoom"
         >
-
-        <input id="init-volume" v-model="volume" :min="minimalVolume" max="100" type="range" />
-      </p>
-      <p>
-        <label for="minimal-volume"
-          >Minimal volume : <span>{{ minimalVolume }}</span></label
-        >
-        <input id="minimal-volume" v-model="minimalVolume" min="0" max="100" type="range" />
-      </p>
-      <p>
-        <label for="dislike-threshold"
-          >Skip song threshold : <span>{{ dislikeThreshold }}</span></label
-        >
-        <input id="dislike-threshold" v-model="dislikeThreshold" min="1" max="5" type="range" />
-      </p>
-      <button type="button" class="" :disabled="hasSameRoomName || roomName.length == 0" @click="createRoom">
-        Create room
-      </button>
-    </form>
-  </div>
+          Create room
+        </button>
+        <button type="button" class="tracking-tighter underline">Enter an existing room!</button>
+      </form>
+    </div>
+  </HallShell>
 </template>
 <script>
 import { mapGetters } from 'vuex'
 import firebase from '../store/firebase.js'
+import HallShell from '../components/hall/HallShell.vue'
 
 export default {
+  components: {
+    HallShell,
+  },
   data() {
     return {
       roomKey: Array.from(window.crypto.getRandomValues(new Uint32Array(2)), item => item.toString(16)).join(''),
@@ -107,3 +100,20 @@ export default {
   },
 }
 </script>
+<style lang="postcss">
+.form {
+  @apply space-y-4;
+  & p {
+    @apply flex flex-col;
+    & label {
+      @apply mb-2;
+    }
+    & input {
+      @apply tracking-[-0.015rem] h-12 border-2 border-black p-2 pl-5 align-middle bg-[#E0E0E0];
+    }
+    &:nth-child(3) input {
+      @apply bg-white;
+    }
+  }
+}
+</style>
