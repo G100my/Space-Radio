b36b7489f94b60856bba7f36a0230ac5660f4d9a
G100my <G100my.lan229@gmail.com>
2021-04-27 23:11:22 +0800

    style: h1 和 content 的間距改用浮動 5vh

--- src/components/hall/HallShell.vue
+++ src/components/hall/HallShell.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="h-full flex flex-col items-center pt-16 sm:pt-44 pb-9 sm:pb-20 px-11">
     <h1 class="text-header font-semibold text-center">Jukebox</h1>
-    <div class="flex-1 pt-24 sm:pt-24 w-full max-w-xs">
+    <div class="flex-1 pt-24 sm:pt-[5vh] w-full max-w-xs">
       <router-view />
     </div>
   </div>
