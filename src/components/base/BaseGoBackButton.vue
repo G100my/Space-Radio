c949fe3c431d9528538d417b0b19ef76084c7bf1
G100my <G100my.lan229@gmail.com>
2021-05-04 22:12:02 +0800

    change icon, use heroicons

--- src/components/base/BaseGoBackButton.vue
+++ src/components/base/BaseGoBackButton.vue
@@ -1,14 +1,14 @@
 <template>
-  <button type="button" class="text-gray-600 flex absolute bottom-full transform -translate-y-5">
-    <IconChevronLeft />
-    <span>Go Back</span>
+  <button type="button" class="text-gray-600 absolute flex items-center w-max bottom-full transform -translate-y-5">
+    <ChevronLeftIcon class="w-8" />
+    <span class="whitespace-nowrap">Go Back</span>
   </button>
 </template>
 <script>
-import IconChevronLeft from '../../assets/icons/chevron-left.vue'
+import { ChevronLeftIcon } from '@heroicons/vue/solid'
 export default {
   components: {
-    IconChevronLeft,
+    ChevronLeftIcon,
   },
 }
 </script>
