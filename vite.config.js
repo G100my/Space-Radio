9d08daa60ef63b363df7978b68bb1aa76a17122d
G100my <G100my.lan229@gmail.com>
2021-06-06 05:10:34 +0800

    feat: set up vite-svg-loader

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
 import vue from '@vitejs/plugin-vue'
+import svgLoader from 'vite-svg-loader'
 
 /**
  * @type {import('vite').UserConfig}
  */
 export default ({ command }) => {
   return {
-    plugins: [vue()],
+    plugins: [vue(), svgLoader()],
     optimizeDeps: {
       include: ['firebase/app', 'firebase/database', 'firebase/analytics'],
       exclude: ['firebase'],
