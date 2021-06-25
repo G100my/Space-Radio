fbfba531c9a80e98fd5d4a679acae8a33c3b6142
G100my <G100my.lan229@gmail.com>
2021-06-26 02:40:13 +0800

    feat: init PlayStatusButton

---
+++ src/components/feature-buttons/PlayStatusButton.vue
@@ -0,0 +1,26 @@
+<script>
+import IconPlay from '@/assets/icons/play.svg'
+import IconStop from '@/assets/icons/stop.svg'
+import { isSpotifyPlayerPaused } from '../../composables/useSpotifyPlayer.js'
+export default {
+  components: {
+    IconPlay,
+    IconStop,
+  },
+  setup() {
+    return isSpotifyPlayerPaused
+  },
+}
+</script>
+<template>
+  <button
+    id="play-status-button"
+    class="flex items-center justify-center h-10 w-10 bg-primary rounded-full"
+    type="button"
+    :disabled="currentActiveDeviceId !== spotifyPlayerId"
+    @click="togglePlay"
+  >
+    <IconPlay v-show="isSpotifyPlayerPaused" />
+    <IconStop v-show="!isSpotifyPlayerPaused" />
+  </button>
+</template>
