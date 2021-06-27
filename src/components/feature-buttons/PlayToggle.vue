86389cc17ad0e7ad7c32c5ada14278aa1a543e50
G100my <G100my.lan229@gmail.com>
2021-06-28 02:46:40 +0800

    feat: PlayToggle add disabled style
    player_state_changed: 增加 確認正在啟用的裝置

--- src/components/feature-buttons/PlayToggle.vue
+++ src/components/feature-buttons/PlayToggle.vue
@@ -1,21 +1,35 @@
 <script>
 import IconPlay from '@/assets/icons/icon-play.svg'
 import IconPause from '@/assets/icons/icon-pause.svg'
-import { isSpotifyPlayerPaused } from '../../composables/useSpotifyPlayer.js'
+import {
+  spotifyPlayer,
+  isSpotifyPlayerPaused,
+  currentActiveDeviceId,
+  spotifyPlayerId,
+} from '@/composables/useSpotifyPlayer.js'
 export default {
   components: {
     IconPlay,
     IconPause,
   },
   setup() {
-    return isSpotifyPlayerPaused
+    // fixme 怎樣直接能在 @click 綁訂 spotifyPlayer.togglePlay() ?
+    function togglePlay() {
+      spotifyPlayer.togglePlay()
+    }
+    return {
+      isSpotifyPlayerPaused,
+      currentActiveDeviceId,
+      spotifyPlayerId,
+      togglePlay,
+    }
   },
 }
 </script>
 <template>
   <button
     id="play-status-button"
-    class="flex items-center justify-center h-10 w-10 bg-primary rounded-full"
+    class="flex items-center justify-center h-10 w-10 bg-primary rounded-full disabled:filter disabled:grayscale"
     type="button"
     :disabled="currentActiveDeviceId !== spotifyPlayerId"
     @click="togglePlay"
