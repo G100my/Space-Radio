9e264e25c8bd35420a250e83c696e0595bab2708
G100my <G100my.lan229@gmail.com>
2021-06-28 00:14:15 +0800

    init LikeButton

---
+++ src/components/feature-buttons/LikeButton.vue
@@ -0,0 +1,14 @@
+<script>
+import IconHeart from '@/assets/icons/icon/like-filled.svg'
+
+export default {
+  components: {
+    IconHeart,
+  },
+}
+</script>
+<template>
+  <button type="button" class="w-10 h-10 flex items-center justify-center">
+    <IconHeart class="text-natural-gray1 w-6 h-6" />
+  </button>
+</template>
