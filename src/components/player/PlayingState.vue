43111e9f2af165505b9d28295cf4ecde848a555d
G100my <G100my.lan229@gmail.com>
2021-06-28 00:14:56 +0800

    feat: update PlayingState laptop style

--- src/components/player/PlayingState.vue
+++ src/components/player/PlayingState.vue
@@ -4,24 +4,26 @@
       <h3 class="text-2xl text-natural-white">Now</h3>
       <img :src="SpotifyLogo" alt="Spotify" class="w-20" />
     </header>
-    <section id="plarer-state" class="mt-7 flex items-start">
-      <div class="relative bg-tertiary-2 bg-opacity-60 px-5 py-3">
+    <section id="plarer-state" class="mt-7 flex items-start laptop:flex-col">
+      <div
+        class="relative bg-tertiary-2 bg-opacity-60 px-5 pt-3 pb-4 laptop:w-10/12 laptop:px-9 laptop:pt-[22px] laptop:pb-7"
+      >
         <a :href="playerPlayingAlbum.url" target="_blank">
           <img
             :src="playerPlayingAlbum.image_url ? playerPlayingAlbum.image_url : logo"
             :alt="playerPlayingAlbum.name"
             :title="playerPlayingAlbum.name"
-            class="h-20 w-20"
+            class="h-20 w-20 laptop:w-full laptop:h-auto"
           />
         </a>
-        <PlayToggle class="absolute right-0 bottom-0 translate-y-1/3 translate-x-1/3" />
+        <PlayToggle
+          class="absolute right-0 bottom-0 translate-y-1/3 translate-x-1/3 laptop:translate-x-3/4 laptop:translate-y-1/2"
+        />
       </div>
-      <section class="flex-1 pl-4 self-stretch flex flex-col">
-        <section class="flex">
+      <section class="flex-1 pl-4 self-stretch flex flex-col laptop:-mt-4 laptop:pl-0 laptop:relative laptop:space-y-2">
+        <section class="flex laptop:w-9/12 laptop:relative">
           <p class="flex-1 font-bold text-natural-gray1">{{ playerPlayingTrackName }}</p>
-          <button type="button" class="w-6 h-6">
-            <IconHeart class="text-natural-gray1" />
-          </button>
+          <LikeButton class="laptop:absolute laptop:-top-2 laptop:right-0 laptop:z-30" />
         </section>
         <section class="mt-1">
           <BaseMarquee>
@@ -30,7 +32,7 @@
             </a>
           </BaseMarquee>
         </section>
-        <section class="mt-auto flex justify-end text-primary">
+        <section class="mt-auto flex justify-end text-primary laptop:justify-between">
           <ProgressTimer class="text-opacity-80" />
           <p class="ml-5">{{ '點播人' }}</p>
         </section>
@@ -137,8 +139,8 @@ import UserLog from './UserLog.vue'
 import logo from '@/assets/vinyl-record.png'
 import ProgressTimer from './ProgressTimer.vue'
 import SpotifyLogo from '@/assets/images/Spotify_Logo_CMYK_Green.png'
-import IconHeart from '@heroicons/vue/solid/HeartIcon'
 import BaseMarquee from '@/components/base/BaseMarquee.vue'
+import LikeButton from '@/components/feature-buttons/LikeButton.vue'
 import PlayToggle from '@/components/feature-buttons/PlayToggle.vue'
 
 export default {
@@ -146,7 +148,7 @@ export default {
     UserLog,
     ProgressTimer,
     HostControl: defineAsyncComponent(() => import('./HostControl.vue')),
-    IconHeart,
+    LikeButton,
     BaseMarquee,
     PlayToggle,
   },
