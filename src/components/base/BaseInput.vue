f5893b80174321375f1f09dad8acb6bf049f4c3c
G100my <G100my.lan229@gmail.com>
2021-07-07 01:35:47 +0800

    feat: init BaseInput

---
+++ src/components/base/BaseInput.vue
@@ -0,0 +1,28 @@
+<script>
+export default {
+  props: {
+    modelValue: {
+      type: [String, Number],
+      default: '',
+    },
+  },
+  emits: ['update:modelValue'],
+}
+</script>
+<template>
+  <div class="base-input">
+    <input v-model="value" v-bind="$attrs" @input="$emit('update:modelValue', $event.target.value)" />
+    <slot />
+  </div>
+</template>
+<style lang="postcss">
+.base-input > input {
+  @apply mt-1.5 w-full bg-tertiary-1 border border-tertiary-2 border-opacity-60 -shadow-4 px-4 py-3 text-natural-gray1 rounded text-body;
+  &:focus {
+    @apply border-primary border-opacity-80 outline-none;
+  }
+}
+.base-input > input::placeholder {
+  @apply text-natural-gray3;
+}
+</style>
