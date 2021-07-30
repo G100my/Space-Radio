66f597165eeac37ac9a6420899ca684af44f043e
G100my <G100my.lan229@gmail.com>
2021-07-31 07:44:29 +0800

    feat(Vote): add host user watch currentDislike

--- src/components/player/Vote.vue
+++ src/components/player/Vote.vue
@@ -1,15 +1,51 @@
 <script>
 import { useStore } from 'vuex'
-import { computed } from 'vue'
+import { computed, watch } from 'vue'
+import { nextTrack } from '@/composables/useSpotifyPlayer'
 
 export default {
   setup() {
     const store = useStore()
+    const currentDislike = computed(() => store.getters.currentDislike)
+    const currentDislikeThreshold = computed(() => store.getters.currentDislikeThreshold)
+    const currentDislikeCountdown = computed(() => store.getters.currentDislikeCountdown)
+    const minimalVolume = computed(() => store.getters.currentMinimalVolume)
+
+    const isHostUser = computed(() => store.getters.isHostUser)
+    const unwatch = watch(isHostUser, newValue => {
+      if (newValue) {
+        let dislikeCountdownTimer
+        watch(currentDislike, newValue => {
+          if (dislikeCountdownTimer) {
+            clearTimeout(dislikeCountdownTimer)
+            dislikeCountdownTimer = null
+          }
+          if (newValue >= currentDislikeThreshold.value) {
+            let counter = 10
+            dislikeCountdownTimer = setInterval(() => {
+              counter -= 1
+              store.dispatch('updateDislikeCountdown', counter)
+              if (counter <= 0) {
+                nextTrack(minimalVolume.value)
+                clearInterval(dislikeCountdownTimer)
+                store.dispatch('clearDislikeVote')
+                store.dispatch('updateDislikeCountdown', false)
+                dislikeCountdownTimer = null
+              }
+            }, 1000)
+          } else if (newValue < currentDislikeThreshold.value && currentDislikeCountdown.value) {
+            store.dispatch('updateDislikeCountdown', false)
+          }
+        })
+      }
+      unwatch()
+    })
+
     return {
       isVoted: computed(() => store.state.PlayingState.isVoted),
-      currentDislike: computed(() => store.getters.currentDislike),
-      currentDislikeThreshold: computed(() => store.getters.currentDislikeThreshold),
-      currentDislikeCountdown: computed(() => store.getters.currentDislikeCountdown),
+      currentDislike,
+      currentDislikeThreshold,
+      currentDislikeCountdown,
       playerPlayingTrackId: computed(() => store.getters.playerPlayingTrackId),
     }
   },
