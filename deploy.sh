169fa451ff596e331c10735f753f53665a218ba7
G100my <G100my.lan229@gmail.com>
2021-07-31 10:08:19 +0800

    feat(deploy): add deploy.sh, remove demo script

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+npm run build
+cd docs
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+git push -f https://github.com/G100my/Jukebox.git main:demo
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+
+cd -
