e61d71b3fd3cc24b5b08fcd9c249a2129d360577
G100my <G100my.lan229@gmail.com>
2021-08-24 15:51:02 +0800

    update useSpotifyPlayer test

--- src/composables/useSpotifyPlayer.test.js
+++ src/composables/useSpotifyPlayer.test.js
@@ -9,7 +9,7 @@ jest.mock('../utility/PKCE.js')
 jest.mock('../utility/tts.js')
 jest.mock('vue')
 
-describe.only('track flow', () => {
+describe('track flow', () => {
   jest.useFakeTimers('legacy')
   it('只發送一次 store.dispatch("sendNextQueue")', () => {
     const handlerMock = jest.fn(setNextQueueTimeoutHandler)
@@ -21,7 +21,9 @@ describe.only('track flow', () => {
     expect(store.dispatch).toBeCalledTimes(1)
     expect(store.dispatch).toBeCalledWith('sendNextQueue')
   })
+})
 
+describe('clearPendingQueueHandler', () => {
   const pendingQueue = computed(() => store.getters.pendingQueue)
   expect(pendingQueue.value).toEqual({})
   const handlerMock = jest.fn(clearPendingQueueHandler)
@@ -33,15 +35,15 @@ describe.only('track flow', () => {
     expect(store.dispatch).toBeCalledTimes(0)
   })
 
+  const nextTrackIndex = testPlayingState.findIndex(item => {
+    return item.track_window.current_track.id !== testPlayingState[0].track_window.current_track.id
+  })
+  expect(nextTrackIndex).toBeGreaterThanOrEqual(0)
+
+  const valuesMock = jest.fn()
+  valuesMock.mockReturnValue({})
+
   it('clearPendingQueueHandler', () => {
-    const nextTrackIndex = testPlayingState.findIndex(item => {
-      return item.track_window.current_track.id !== testPlayingState[0].track_window.current_track.id
-    })
-    expect(nextTrackIndex).toBeGreaterThanOrEqual(0)
-
-    const valuesMock = jest.fn()
-    valuesMock.mockReturnValue({})
-
     for (let i = 0; i < nextTrackIndex + 6; i++) {
       const pending = valuesMock()
       handlerMock(testPlayingState[i], pending)
@@ -49,6 +51,25 @@ describe.only('track flow', () => {
         valuesMock.mockReturnValue({
           dfsdfsfsfsdf: {
             track_id: testPlayingState[nextTrackIndex].track_window.current_track.id,
+          },
+        })
+      }
+    }
+
+    handlerMock(testPlayingState[0], pendingQueue.value)
+    expect(store.dispatch).toBeCalledTimes(1)
+  })
+
+  it('clearPendingQueueHandler, has linked_from', () => {
+    const SPECIAL_TRACK_ID = '6KuU2rnFYUyYzGOwcCZDT7'
+
+    for (let i = 0; i < nextTrackIndex + 6; i++) {
+      const pending = valuesMock()
+      handlerMock(testPlayingState[i], pending)
+      if (i === nextTrackIndex) {
+        valuesMock.mockReturnValue({
+          dfsdfsfsfsdf: {
+            track_id: SPECIAL_TRACK_ID,
           },
         })
       }
