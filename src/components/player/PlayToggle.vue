9d42ad62be79dd2cba7c4052410af0518b83a8ad
G100my <G100my.lan229@gmail.com>
2021-08-25 00:05:17 +0800

    refactor(PlayToggle): remove useless if condition

--- src/components/player/PlayToggle.vue
+++ src/components/player/PlayToggle.vue
@@ -7,29 +7,23 @@ import {
   spotifyPlayerId,
   togglePlay,
 } from '@/composables/useSpotifyPlayer.js'
-import { computed } from 'vue'
-import { useStore } from 'vuex'
 export default {
   components: {
     IconPlay,
     IconPause,
   },
   setup() {
-    const store = useStore()
-
     return {
       isSpotifyPlayerPaused,
       currentActiveDeviceId,
       spotifyPlayerId,
       togglePlay,
-      isHost: computed(() => store.getters.isHostUser),
     }
   },
 }
 </script>
 <template>
   <button
-    v-if="isHost"
     id="play-status-button"
     class="flex items-center justify-center h-10 w-10 bg-primary rounded-full focus:outline-none focus:ring-2 focus:ring-natural-gray1"
     :class="{ 'filter grayscale': currentActiveDeviceId !== spotifyPlayerId }"
