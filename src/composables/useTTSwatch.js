271ae96bb13f05b04904ae712d4a68aa118f57b6
G100my <G100my.lan229@gmail.com>
2021-08-26 03:15:46 +0800

    style: combine useTTSwatch.js with tts.js

--- src/composables/useTTSwatch.js
+++ src/composables/useTTSwatch.js
@@ -1,7 +1,39 @@
 import store from '@/store/index.js'
 import { computed, watch } from 'vue'
 import { messageOutputMaker } from '../utility/messageOutputMaker.js'
-import { TTS } from '../utility/tts.js'
+
+const utterance = new window.SpeechSynthesisUtterance()
+utterance.pitch = 1
+utterance.rate = 0.75
+utterance.volume = 1
+utterance.lang = 'zh-TW'
+
+function setTTSVoice() {
+  const voice = speechSynthesis.getVoices().find(item => item.name.includes('Google') && item.lang.includes('zh-TW'))
+  if (voice !== null) utterance.voice = voice
+}
+
+speechSynthesis.onvoiceschanged = () => {
+  if (!utterance.voice) setTTSVoice()
+}
+
+function TTS(text) {
+  return new Promise((resolve, reject) => {
+    utterance.onerror = error => {
+      console.error('utterance error: ', error)
+      reject()
+    }
+    utterance.onend = () => {
+      console.log('utterance end')
+      resolve()
+    }
+    if (utterance.voice === null) setTTSVoice()
+    utterance.text = text
+    speechSynthesis.speak(utterance)
+  })
+}
+
+export { TTS }
 
 const pendingOrder = computed(() => store.getters.pendingOrder)
 
