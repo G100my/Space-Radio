0cf8bd571de5a3d912e63a927b8e2a2c1c5b4dd6
G100 <G100my.lan229@gmail.com>
2021-10-24 21:31:42 +0800

    update Hall nav

--- src/components/hall/HallNav.vue
+++ src/components/hall/HallNav.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="my-auto w-full flex items-center space-x-5">
-    <a href="##">Terms & privacy</a>
-    <a href="##">Contact us</a>
+    <a href="https://echowenchi.myportfolio.com/work" target="_blank"
+      >Designer: <strong class="text-primary">Echo</strong></a
+    >
+    <a href="https://g100my.github.io/" target="_blank">Developer: <strong class="text-primary">G100</strong></a>
   </div>
 </template>
