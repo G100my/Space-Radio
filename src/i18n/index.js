6c6a3a062e8382adbbe7719336f4f94a42a2050b
G100 <g100my.lan229@gmail.com>
2022-02-13 20:27:49 +0800

    style: change default locale

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -3,8 +3,8 @@ import en from './en.json'
 import zh from './zh-TW.json'
 
 export default createI18n({
-  locale: 'zh-TW',
-  fallbackLocale: 'en',
+  locale: 'en',
+  fallbackLocale: 'zh-TW',
   legacy: false,
   messages: {
     en,
