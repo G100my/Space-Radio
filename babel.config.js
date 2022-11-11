97a0d9f500ecbcc890cd977efed32096e94d103c
G100 <g100my.lan229@gmail.com>
2022-11-11 13:47:26 +0800

    core: remove useless

--- babel.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  // preset 順序是後項為先
-  presets: [
-    ['@babel/preset-env', { targets: { node: 'current' }, debug: true }],
-    ['babel-preset-vite', { env: true, glob: false }],
-  ],
-}
