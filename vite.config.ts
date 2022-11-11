4742650b81a19d18e580d76b544eb3998fa0e2ef
G100 <g100my.lan229@gmail.com>
2022-11-11 13:47:51 +0800

    core: update vite.config to .ts

---
+++ vite.config.ts
@@ -0,0 +1,33 @@
+import { fileURLToPath, URL } from 'node:url'
+import svgLoader from 'vite-svg-loader'
+import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite'
+import path from 'path'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig(({ command }) => ({
+  optimizeDeps: {
+    include: ['firebase/app', 'firebase/database', 'firebase/analytics'],
+    exclude: ['firebase'],
+  },
+  base: command === 'build' ? '/Space-Radio/' : './',
+  build: {
+    outDir: './docs/',
+  },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  plugins: [
+    vue(),
+    svgLoader(),
+    VueI18nPlugin({
+      defaultSFCLang: 'yaml',
+      globalSFCScope: true,
+      include: path.resolve(__dirname, './src/i18n/**'),
+    }),
+  ],
+}))
