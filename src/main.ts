44c3adcec10e7aa6acdc8047121b8e569e0e0432
G100 <g100my.lan229@gmail.com>
2022-11-11 14:02:12 +0800

    core: migrate main.js->main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,16 @@
 import { createApp } from 'vue'
+// import { createPinia } from 'pinia'
+
 import App from './App.vue'
 import router from './router'
 import store from './store/'
 import './style/index.css'
 import i18n from './i18n'
 
-if (import.meta.env.PROD) {
-  console.log = () => {}
-}
-
 // prettier-ignore
 createApp(App)
   .use(router)
   .use(store)
+  // .use(createPinia())
   .use(i18n)
   .mount('#app')
