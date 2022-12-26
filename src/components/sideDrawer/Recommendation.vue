7f9f1ebc757cf96502e4680d3d55ff6aaf84b623
G100 <g100my.lan229@gmail.com>
2022-12-27 01:08:59 +0800

    feat: update Recommendation style

--- src/components/sideDrawer/Recommendation.vue
+++ src/components/sideDrawer/Recommendation.vue
@@ -5,51 +5,58 @@ import { useI18n } from 'vue-i18n'
 import { reactive, ref, shallowReactive, watch } from 'vue'
 import { spotifyAPI } from '@/plugins/spotifyAPI'
 import { spotifyCoverPicker } from '@/utility/dataFormat'
-import fakedata from '@/fakeData/search.json'
-import BaseMarquee from '../base/BaseMarquee.vue'
+import BaseMarquee from '@/components/base/BaseMarquee.vue'
+import BaseRadio from '@/components/base/BaseRadio.vue'
 import IconSearch from '@/assets/icons/icon-search.svg?component'
 import IconMinusRaw from '@/assets/icons/icon-minus.svg?raw'
 import IconPlusRaw from '@/assets/icons/icon-plus.svg?raw'
 import IconPlus from '@/assets/icons/icon-plus.svg?component'
-import { TabGroup, TabList, Tab, TabPanels, TabPanel } from '@headlessui/vue'
 import { usePersonalPlaylistStore } from '@/store'
+
+type SearckType = Extract<Parameters<typeof spotifyAPI.search>[1][number], 'track' | 'artist'>
+interface SearchSeed {
+  key: string
+  name: string
+}
 
 export default {
   name: 'Recommendation',
-  components: { BaseMarquee, IconSearch, IconPlus, TabGroup, TabList, TabPanel, TabPanels, Tab },
+  components: {
+    BaseMarquee,
+    BaseRadio,
+    IconSearch,
+    IconPlus,
+  },
   emits: ['activeSideDrawer'],
   setup() {
     const { t } = useI18n()
     const autherSeeds = ref([])
-    const keywordArtist = ref('')
+    const keyword = ref('')
     const seed = reactive({
-      artists: new Map(),
-      tracks: new Map(),
+      artist: [] as SearchSeed[],
+      track: [] as SearchSeed[],
     })
 
-    const results = shallowReactive({
-      artists:
-        fakedata.artists.map(i => {
-          console.log(i)
-          return i
-        }) ?? [],
-      tracks: fakedata.tracks ?? [],
+    const results = shallowReactive<{ artists: SpotifyApi.ArtistObjectFull[]; tracks: SpotifyApi.TrackObjectFull[] }>({
+      artists: [],
+      tracks: [],
     })
-    async function handleSearch(type) {
-      const response = await spotifyAPI.search(keywordArtist.value, [type])
-      console.log(response)
-      switch (type) {
+
+    const searchType = ref<SearckType>('track')
+    async function handleSearch() {
+      if (!keyword.value) return
+      const response = await spotifyAPI.search(keyword.value, [searchType.value])
+      switch (searchType.value) {
         case 'artist':
-          results.artists = response.artists?.items ?? []
+          results.artists = response.artists!.items
           break
         case 'track':
-          results.tracks = response.tracks?.items ?? []
+          results.tracks = response.tracks!.items
           break
       }
     }
-    const handleSearchArtist = () => handleSearch('artist')
-    const handleSearchTrack = () => handleSearch('track')
 
+    const genres = ref<string[]>([])
     function fetchGenreSeeds() {
       return spotifyAPI.getAvailableGenreSeeds().then(response => {
         if (!response) {
@@ -60,8 +67,7 @@ export default {
         genres.value = response.genres
       })
     }
-    const genres = ref([])
-    const genresRecord = JSON.parse(localStorage.getItem('spaceradio_genres'))
+    const genresRecord = JSON.parse(localStorage.getItem('spaceradio_genres') ?? '[]')
     if (genresRecord) {
       const isOverOneDay = Date.now() - Number(genresRecord.timestamp) > 24 * 60 * 60 * 1000
       if (isOverOneDay) fetchGenreSeeds()
@@ -70,17 +76,25 @@ export default {
       fetchGenreSeeds()
     }
 
-    function addToggle(event, type, key, value) {
+    function addToggle(
+      event: MouseEvent,
+      type: SearckType,
+      key: string /** track id / artist id */,
+      name: string /** track name/artist name */
+    ) {
       console.log(arguments)
-      const target = event.currentTarget
-      if (!target.dataset.added) {
-        target.dataset.added = true
-        target.innerHTML = IconMinusRaw
-        seed[type].set(key, value)
+      const element = event.currentTarget as HTMLButtonElement
+      if (!element.dataset.added) {
+        element.dataset.added = 'true'
+        element.innerHTML = IconMinusRaw
+        seed[type].push({ key, name })
       } else {
-        delete target.dataset.added
-        target.innerHTML = IconPlusRaw
-        seed[type].delete(key, value)
+        delete element.dataset.added
+        element.innerHTML = IconPlusRaw
+        const targetIndex = seed[type].findIndex(i => i.key === key)
+        if (targetIndex !== -1) {
+          seed[type].splice(targetIndex, 1)
+        }
       }
     }
     watch(seed, newOne => {
@@ -92,110 +106,86 @@ export default {
       spotify: computed(() => usePersonalPlaylistStore().spotifyPlaylists),
       t,
       autherSeeds,
-      keyword: keywordArtist,
-      handleSearchArtist,
-      handleSearchTrack,
+      keyword: keyword,
       results,
       spotifyCoverPicker,
       addToggle,
+      fetchGenreSeeds,
+      handleSearch,
+      searchType,
+      seedArtist: computed(() => Array.from(seed.artist)),
+      seedTrack: computed(() => Array.from(seed.track)),
     }
   },
 }
 </script>
 <template>
-  <div class="flex h-full flex-col text-green-200">
+  <div class="flex h-full w-96 flex-col bg-blue-900">
     <header>
-      <h2 class="text-subtitle text-natural-gray1 laptop:text-header">{{ t('add_from_recommendation') }}</h2>
+      <h2 class="text-subtitle text-natural-gray1 laptop:text-header">{{ $t('add_from_recommendation') }}</h2>
     </header>
 
     <div class="mt-4 flex-1 flex-col overflow-hidden">
-      <div class="flex h-full flex-col overflow-y-hidden">
+      <div class="flex h-full flex-col overflow-y-hidden text-natural-white">
         <h3 class="mb-4 flex justify-between font-bold">
-          <!-- <p class="text-natural-white">{{ t('your_library') }}</p> -->
           <img class="w-20" :src="SpotifyLogo" alt="Spotify logo" />
         </h3>
-        <form class="flex flex-col space-y-2 bg-red-400">
-          <div class="flex items-center space-x-2">
-            <label for="search_artist">Artist</label>
-            <input
-              id="search_artist"
-              v-model="keyword"
-              class="base-input"
-              type="text"
-              @keydown.enter="handleSearchArtist"
-            />
-            <button type="button" class="btn-tertiary" @click="handleSearchArtist"><IconSearch /></button>
-          </div>
-          <div class="flex items-center space-x-2">
-            <label for="search_track">Track</label>
-            <input
-              id="search_track"
-              v-model="keyword"
-              class="base-input"
-              type="text"
-              @keydown.enter="handleSearchTrack"
-            />
-            <button type="button" class="btn-tertiary" @click="handleSearchTrack"><IconSearch /></button>
-          </div>
-        </form>
-        <!--  -->
-        <TabGroup as="div" class="h-0 flex-1 overflow-auto">
-          <TabList class="mx-auto flex w-2/3 justify-center space-x-1">
-            <Tab v-for="resultType in ['tracks', 'artists']" :key="resultType" v-slot="{ selected }" as="template">
-              <button class="btn-secondary" :class="{ 'bg-tertiary-1': selected }">
-                {{ resultType }}
-              </button>
-            </Tab>
-          </TabList>
+        <fieldset class="flex items-center space-x-2">
+          <input v-model="keyword" class="base-input" type="text" @keydown.enter="handleSearch" />
+          <button type="button" class="btn-tertiary" @click="handleSearch"><IconSearch /></button>
+        </fieldset>
+        <fieldset class="flex gap-4">
+          <BaseRadio name="searchType" value="artist" :label="$t('artists')" v-model="searchType" />
+          <BaseRadio name="searchType" value="track" :label="$t('tracks')" v-model="searchType" />
+        </fieldset>
 
-          <TabPanels class="mt-2">
-            <TabPanel>
-              <ul class="h-full overflow-auto">
-                <li v-for="artist in results.artists" :key="artist.id" class="flex items-center space-x-3 py-1 px-4">
-                  <img class="_cover" :src="spotifyCoverPicker(artist.images)" alt="" />
-                  <div class="w-0 flex-1">
-                    <div>
-                      <BaseMarquee>{{ artist.name }}</BaseMarquee>
-                      <p class="text-xs">Popularity: {{ artist.popularity }}%</p>
-                    </div>
-                    <BaseMarquee>
-                      <span class="w-full space-x-2 text-xs">
-                        <span v-for="genre in artist.genres" :key="genre" class="capitalize">{{ genre }},</span>
-                      </span>
-                    </BaseMarquee>
-                  </div>
-                  <button
-                    type="button"
-                    class="btn-tertiary"
-                    @click="addToggle($event, 'artists', artist.id, artist.name)"
-                  >
-                    <IconPlus />
-                  </button>
-                </li>
-              </ul>
-            </TabPanel>
-            <TabPanel>
-              <ul class="h-full overflow-auto">
-                <li v-for="track in results.tracks" :key="track.id" class="flex items-center space-x-3 py-1 px-4">
-                  <img class="_cover" :src="spotifyCoverPicker(track.album.images)" alt="" />
-                  <div class="w-0 flex-1">
-                    <BaseMarquee>{{ track.name }}</BaseMarquee>
-                    <BaseMarquee>
-                      <span class="w-full space-x-2 text-xs">
-                        <span v-for="artist in track.artists" :key="artist.name" class="capitalize">{{
-                          artist.name
-                        }}</span>
-                      </span>
-                    </BaseMarquee>
-                  </div>
-                  <button type="button" class="btn-tertiary" @click="addToggle($event, 'tracks', track.id, track.name)">
-                    <IconPlus />
-                  </button>
-                </li>
-              </ul>
-            </TabPanel>
-          </TabPanels>
-        </TabGroup>
+        <div class="h-20 border border-natural-gray1">
+          <p>{{ $t('recommendation.search_seed') }}</p>
+          <p v-if="searchType === 'artist'">
+            <span v-for="seed in seedArtist" :key="seed.key" class="seed_word">{{ seed.name }}</span>
+          </p>
+          <p v-else>
+            <span v-for="seed in seedTrack" :key="seed.key" class="seed_word">{{ seed.name }}</span>
+          </p>
+        </div>
+
+        <ul class="h-full overflow-auto">
+          <template v-if="searchType === 'artist'">
+            <li v-for="artist in results.artists" :key="artist.id" class="flex items-center space-x-3 py-1 px-4">
+              <img class="_cover" :src="spotifyCoverPicker(artist.images)" alt="" />
+              <div class="w-0 flex-1">
+                <div>
+                  <BaseMarquee>{{ artist.name }}</BaseMarquee>
+                  <p class="text-xs">Popularity: {{ artist.popularity }}%</p>
+                </div>
+                <BaseMarquee>
+                  <span class="w-full space-x-2 text-xs">
+                    <span v-for="genre in artist.genres" :key="genre" class="capitalize">{{ genre }},</span>
+                  </span>
+                </BaseMarquee>
+              </div>
+              <button type="button" class="btn-tertiary" @click="addToggle($event, 'artist', artist.id, artist.name)">
+                <IconPlus />
+              </button>
+            </li>
+          </template>
+          <template v-if="searchType === 'track'">
+            <li v-for="track in results.tracks" :key="track.id" class="flex items-center space-x-3 py-1 px-4">
+              <img class="_cover" :src="spotifyCoverPicker(track.album.images)" alt="" />
+              <div class="w-0 flex-1">
+                <BaseMarquee>{{ track.name }}</BaseMarquee>
+                <BaseMarquee>
+                  <span class="w-full space-x-2 text-xs">
+                    <span v-for="artist in track.artists" :key="artist.name" class="capitalize">{{ artist.name }}</span>
+                  </span>
+                </BaseMarquee>
+              </div>
+              <button type="button" class="btn-tertiary" @click="addToggle($event, 'track', track.id, track.name)">
+                <IconPlus />
+              </button>
+            </li>
+          </template>
+        </ul>
         <div class="pt-8 pb-10">
           <button type="button" class="btn-primary mx-auto" @click="fetchGenreSeeds">Get Recommendations</button>
         </div>
@@ -203,29 +193,14 @@ export default {
     </div>
   </div>
 </template>
-<style lang="postcss" scoped>
-._side_drawer_ul {
-  @apply flex-1 space-y-4 overflow-y-auto;
-  li {
-    @apply flex min-h-[70px] cursor-pointer items-center justify-between rounded-10 bg-tertiary-1 py-2 pl-4 pr-10;
-    & > svg {
-      @apply transition;
-    }
-    &:hover > svg {
-      @apply translate-x-4 transform;
-    }
-  }
-  p {
-    @apply text-natural-white;
-  }
+<style>
+.seed_word::before {
+  content: '"';
 }
-</style>
-<style lang="postcss">
-._overwrite + ._slide_navigation._container {
-  @apply mt-5 h-fit bg-transparent;
-  & > ._slide_navigation_bar {
-    top: auto;
-    bottom: 0;
-  }
+.seed_word::after {
+  content: '"';
+}
+.seed_word:not(:last-child)::after {
+  content: '", ';
 }
 </style>
