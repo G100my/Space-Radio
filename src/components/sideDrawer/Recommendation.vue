2477efb6e93d7ff276f1beadb35fb9cd0e3e77e3
G100 <g100my.lan229@gmail.com>
2022-12-28 17:41:31 +0800

    feat: update Recommendation

--- src/components/sideDrawer/Recommendation.vue
+++ src/components/sideDrawer/Recommendation.vue
@@ -8,9 +8,6 @@ import { spotifyCoverPicker } from '@/utility/dataFormat'
 import BaseMarquee from '@/components/base/BaseMarquee.vue'
 import BaseRadio from '@/components/base/BaseRadio.vue'
 import IconSearch from '@/assets/icons/icon-search.svg?component'
-import IconMinusRaw from '@/assets/icons/icon-minus.svg?raw'
-import IconPlusRaw from '@/assets/icons/icon-plus.svg?raw'
-import IconPlus from '@/assets/icons/icon-plus.svg?component'
 import { usePersonalPlaylistStore } from '@/store'
 
 type SearckType = Extract<Parameters<typeof spotifyAPI.search>[1][number], 'track' | 'artist'>
@@ -25,7 +22,6 @@ export default {
     BaseMarquee,
     BaseRadio,
     IconSearch,
-    IconPlus,
   },
   emits: ['activeSideDrawer'],
   setup() {
@@ -42,7 +38,7 @@ export default {
       tracks: [],
     })
 
-    const searchType = ref<SearckType>('track')
+    const searchType = ref<SearckType>('artist')
     async function handleSearch() {
       if (!keyword.value) return
       const response = await spotifyAPI.search(keyword.value, [searchType.value])
@@ -55,6 +51,10 @@ export default {
           break
       }
     }
+    function handleDeleteSeed(index: number) {
+      if (searchType.value === 'artist') seed.artist.splice(index, 1)
+      else seed.track.splice(index, 1)
+    }
 
     const genres = ref<string[]>([])
     function fetchGenreSeeds() {
@@ -64,6 +64,7 @@ export default {
           return
         }
         localStorage.setItem('spaceradio_genres', JSON.stringify({ genres: response.genres, timestamp: Date.now() }))
+        console.log(response)
         genres.value = response.genres
       })
     }
@@ -77,20 +78,13 @@ export default {
     }
 
     function addToggle(
-      event: MouseEvent,
       type: SearckType,
       key: string /** track id / artist id */,
       name: string /** track name/artist name */
     ) {
-      console.log(arguments)
-      const element = event.currentTarget as HTMLButtonElement
-      if (!element.dataset.added) {
-        element.dataset.added = 'true'
-        element.innerHTML = IconMinusRaw
+      if (seed[type].findIndex(i => i.key === key) === -1) {
         seed[type].push({ key, name })
       } else {
-        delete element.dataset.added
-        element.innerHTML = IconPlusRaw
         const targetIndex = seed[type].findIndex(i => i.key === key)
         if (targetIndex !== -1) {
           seed[type].splice(targetIndex, 1)
@@ -113,41 +107,48 @@ export default {
       fetchGenreSeeds,
       handleSearch,
       searchType,
-      seedArtist: computed(() => Array.from(seed.artist)),
-      seedTrack: computed(() => Array.from(seed.track)),
+      seedList: computed(() => (searchType.value === 'artist' ? Array.from(seed.artist) : Array.from(seed.track))),
+      handleDeleteSeed,
     }
   },
 }
 </script>
 <template>
-  <div class="flex h-full w-96 flex-col bg-blue-900">
+  <div class="flex h-full flex-col">
     <header>
       <h2 class="text-subtitle text-natural-gray1 laptop:text-header">{{ $t('add_from_recommendation') }}</h2>
     </header>
 
     <div class="mt-4 flex-1 flex-col overflow-hidden">
-      <div class="flex h-full flex-col overflow-y-hidden text-natural-white">
+      <div class="flex h-full flex-col gap-4 overflow-y-hidden text-natural-white">
         <h3 class="mb-4 flex justify-between font-bold">
           <img class="w-20" :src="SpotifyLogo" alt="Spotify logo" />
         </h3>
-        <fieldset class="flex items-center space-x-2">
-          <input v-model="keyword" class="base-input" type="text" @keydown.enter="handleSearch" />
+        <fieldset class="flex w-full items-center space-x-2">
+          <input v-model="keyword" class="base-input flex-1" type="text" @keydown.enter="handleSearch" />
           <button type="button" class="btn-tertiary" @click="handleSearch"><IconSearch /></button>
         </fieldset>
-        <fieldset class="flex gap-4">
+        <fieldset class="flex justify-end gap-4">
           <BaseRadio name="searchType" value="artist" :label="$t('artists')" v-model="searchType" />
           <BaseRadio name="searchType" value="track" :label="$t('tracks')" v-model="searchType" />
         </fieldset>
 
-        <div class="h-20 border border-natural-gray1">
-          <p>{{ $t('recommendation.search_seed') }}</p>
-          <p v-if="searchType === 'artist'">
-            <span v-for="seed in seedArtist" :key="seed.key" class="seed_word">{{ seed.name }}</span>
+        <fieldset class="min-h-fit h-40 border border-natural-gray1 p-2">
+          <legend class="ml-1">{{ $t('recommendation.search_seed') }}</legend>
+          <p>
+            <button
+              v-for="(seed, index) in seedList"
+              :key="seed.key"
+              class="seed_word group"
+              @click="handleDeleteSeed(index)"
+            >
+              <span>{{ seed.name }}</span>
+              <span class="absolute inset-0 bg-natural-black/80 opacity-0 transition-opacity group-hover:opacity-100">
+                <i class="ri-delete-bin-line"></i>
+              </span>
+            </button>
           </p>
-          <p v-else>
-            <span v-for="seed in seedTrack" :key="seed.key" class="seed_word">{{ seed.name }}</span>
-          </p>
-        </div>
+        </fieldset>
 
         <ul class="h-full overflow-auto">
           <template v-if="searchType === 'artist'">
@@ -164,8 +165,13 @@ export default {
                   </span>
                 </BaseMarquee>
               </div>
-              <button type="button" class="btn-tertiary" @click="addToggle($event, 'artist', artist.id, artist.name)">
-                <IconPlus />
+              <button
+                type="button"
+                class="btn-tertiary group text-xl"
+                @click="addToggle('artist', artist.id, artist.name)"
+              >
+                <i v-if="seedList.findIndex(i => artist.id === i.key) === -1" class="ri-add-line" />
+                <i v-else class="ri-subtract-fill" />
               </button>
             </li>
           </template>
@@ -180,8 +186,13 @@ export default {
                   </span>
                 </BaseMarquee>
               </div>
-              <button type="button" class="btn-tertiary" @click="addToggle($event, 'track', track.id, track.name)">
-                <IconPlus />
+              <button
+                type="button"
+                class="btn-tertiary group text-xl"
+                @click="addToggle('track', track.id, track.name)"
+              >
+                <i v-if="seedList.findIndex(i => track.id === i.key) === -1" class="ri-add-line" />
+                <i v-else class="ri-subtract-fill" />
               </button>
             </li>
           </template>
@@ -194,6 +205,9 @@ export default {
   </div>
 </template>
 <style>
+.seed_word {
+  @apply relative overflow-hidden rounded-full;
+}
 .seed_word::before {
   content: '"';
 }
