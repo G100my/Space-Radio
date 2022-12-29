f0a00472d885cfe703a9cccf998b4742c80b6e01
G100 <g100my.lan229@gmail.com>
2022-12-30 05:19:05 +0800

    feat: init BaseDetails

---
+++ src/components/base/BaseDetails.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+defineProps<{
+  summaryText: string
+}>()
+</script>
+<template>
+  <details class="relative h-8 overflow-auto transition-[height] open:h-72">
+    <summary class="sticky top-0 bg-tertiary-2 p-1">{{ summaryText }}</summary>
+    <slot />
+  </details>
+</template>
