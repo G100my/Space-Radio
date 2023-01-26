4488a130c03d0f6cea7ffc134f82fef5a5899cf6
G100 <g100my.lan229@gmail.com>
2023-01-26 17:59:08 +0800

    style: Recommendation layout

--- src/components/sideDrawer/Recommendation.vue
+++ src/components/sideDrawer/Recommendation.vue
@@ -2,11 +2,13 @@
 import SpotifyLogo from '@/assets/images/Spotify_Logo_CMYK_Green.png'
 import { reactive, ref, shallowReactive, onMounted } from 'vue'
 import { spotifyAPI } from '@/plugins/spotifyAPI'
-import { spotifyCoverPicker } from '@/utility/dataFormat'
+import { spotifyCoverPicker, playlistTrackFormater, type FormattedTrack } from '@/utility/dataFormat'
 import BaseMarquee from '@/components/base/BaseMarquee.vue'
 import BaseRadio from '@/components/base/BaseRadio.vue'
 import IconSearch from '@/assets/icons/icon-search.svg?component'
 import BaseDetails from '@/components/base/BaseDetails.vue'
+import TrackListContainer from './TrackListContainer.vue'
+import { useQueueStore } from '@/store'
 
 type SearchType = Extract<Parameters<typeof spotifyAPI.search>[1][number], 'track' | 'artist'>
 type SeedType = 'seed_artists' | 'seed_genres' | 'seed_tracks'
@@ -24,7 +26,9 @@ const results = shallowReactive<{ artists: SpotifyApi.ArtistObjectFull[]; tracks
 })
 
 const searchType = ref<SearchType>('artist')
+const showRecommendation = ref(true)
 async function handleSearch() {
+  showRecommendation.value = false
   if (!keyword.value) return
   const response = await spotifyAPI.search(keyword.value, [searchType.value])
   switch (searchType.value) {
@@ -40,6 +44,7 @@ async function handleSearch() {
 const seeds = reactive<SearchSeed[]>([])
 const findSeedIndex = (key: string, type: SeedType): number => seeds.findIndex(i => i.key === key && i.type === type)
 const seedLimit = 5
+const recommendationList = ref<FormattedTrack[]>([])
 function toggleSeed(
   key: string /** track id / artist id */,
   name: string /** track name/artist name */,
@@ -110,7 +115,22 @@ function getRecommendation() {
   Object.keys(params).forEach(i => {
     if (!params[i as keyof typeof params]) delete params[i as keyof typeof params]
   })
-  spotifyAPI.getRecommendations({ ...params, ...formatted }).then(res => console.log(res))
+  spotifyAPI.getRecommendations({ ...params, ...formatted }).then(res => {
+    recommendationList.value = res.tracks.map(playlistTrackFormater)
+    showRecommendation.value = true
+  })
+}
+const store = useQueueStore()
+function addAll() {
+  const [ids, names] = recommendationList.value.reduce<[string[], string[]]>(
+    (acc, i) => {
+      acc[0].push(i.id)
+      acc[1].push(i.name)
+      return acc
+    },
+    [[], []]
+  )
+  store.addMultiple(ids, names)
 }
 </script>
 <template>
@@ -121,60 +141,64 @@ function getRecommendation() {
         <img class="w-20" :src="SpotifyLogo" alt="Spotify logo" />
       </h2>
     </header>
-
-    <div class="mt-4 flex-1 flex-col overflow-hidden">
-      <div class="flex flex-col overflow-y-hidden">
-        <fieldset class="flex max-h-full w-full flex-wrap items-center gap-2">
-          <input v-model="keyword" class="base-input flex-1" type="text" @keydown.enter="handleSearch" />
-          <button type="button" class="btn-tertiary" @click="handleSearch"><IconSearch /></button>
-          <div class="flex gap-4">
-            <BaseRadio name="searchType" value="artist" :label="$t('recommendation.by_artist')" v-model="searchType" />
-            <BaseRadio name="searchType" value="track" :label="$t('recommendation.by_track')" v-model="searchType" />
-          </div>
-        </fieldset>
-        <fieldset class="my-1 overflow-hidden rounded">
-          <BaseDetails :summaryText="$t('recommendation.genre')">
-            <div class="overflow-auto">
-              <ul class="columns-2 laptop:columns-3">
-                <li v-for="genre in genres" :key="genre">
-                  <button
-                    :class="{ 'bg-neutral-100/30': findSeedIndex(genre, 'seed_genres') !== -1 }"
-                    class="hover:bg-neutral-100/30"
-                    @click="toggleSeed(genre, genre, 'seed_genres')"
-                  >
-                    {{ genre }}
-                  </button>
-                </li>
-              </ul>
-            </div>
-          </BaseDetails>
-        </fieldset>
-
-        <fieldset class="relative h-fit min-h-[65px] border border-natural-gray1 p-2">
-          <legend class="ml-1">{{ $t('recommendation.search_seed') }}</legend>
-          <p>
-            <button
-              v-for="(seed, index) in seeds"
-              :key="typeof seed === 'string' ? seed : seed.key"
-              class="seed_word group"
-              @click="seeds.splice(index, 1)"
-            >
-              <span>{{ typeof seed === 'string' ? seed : seed.name }}</span>
-              <span class="absolute inset-0 bg-natural-black/80 opacity-0 transition-opacity group-hover:opacity-100">
-                <i class="ri-delete-bin-line"></i>
-              </span>
-            </button>
-          </p>
-          <button v-show="seeds.length" class="btn absolute top-0 right-0 bottom-0 my-auto" @click="seeds.length = 0">
-            <i class="ri-delete-bin-line text-2xl" />
-          </button>
-        </fieldset>
-        <div>
-          <button type="button" class="btn-primary mx-auto" @click="getRecommendation">Get Recommendations</button>
+    <div class="mt-2 flex flex-col overflow-y-hidden">
+      <fieldset class="flex max-h-full w-full flex-wrap items-center gap-2">
+        <input v-model="keyword" class="base-input flex-1" type="text" @keydown.enter="handleSearch" />
+        <button type="button" class="btn-tertiary" @click="handleSearch"><IconSearch /></button>
+        <div class="flex gap-4">
+          <BaseRadio name="searchType" value="artist" :label="$t('recommendation.by_artist')" v-model="searchType" />
+          <BaseRadio name="searchType" value="track" :label="$t('recommendation.by_track')" v-model="searchType" />
         </div>
-      </div>
+      </fieldset>
 
-      <ul class="h-full overflow-auto laptop:columns-2">
+      <fieldset class="my-1 overflow-hidden rounded">
+        <BaseDetails :summaryText="$t('recommendation.genre')">
+          <ul class="columns-2 pb-3 laptop:columns-3">
+            <li v-for="genre in genres" :key="genre">
+              <button
+                :class="{ 'bg-neutral-100/30': findSeedIndex(genre, 'seed_genres') !== -1 }"
+                class="capitalize hover:bg-neutral-100/30"
+                @click="toggleSeed(genre, genre, 'seed_genres')"
+              >
+                {{ genre }}
+              </button>
+            </li>
+          </ul>
+        </BaseDetails>
+      </fieldset>
+
+      <fieldset class="relative h-fit min-h-[65px] border border-natural-gray1 p-2">
+        <legend class="ml-1">{{ $t('recommendation.search_seed') }}</legend>
+        <p>
+          <button
+            v-for="(seed, index) in seeds"
+            :key="typeof seed === 'string' ? seed : seed.key"
+            class="seed_word group"
+            @click="seeds.splice(index, 1)"
+          >
+            <span>{{ typeof seed === 'string' ? seed : seed.name }}</span>
+            <span class="absolute inset-0 bg-natural-black/80 opacity-0 transition-opacity group-hover:opacity-100">
+              <i class="ri-delete-bin-line"></i>
+            </span>
+          </button>
+        </p>
+        <button v-show="seeds.length" class="btn absolute top-0 right-0 bottom-0 my-auto" @click="seeds.length = 0">
+          <i class="ri-delete-bin-line text-2xl" />
+        </button>
+      </fieldset>
+
+      <div class="my-4 flex justify-end gap-4">
+        <button v-show="recommendationList.length" type="button" class="btn-secondary" @click="addAll">
+          {{ $t('add_all') }}
+        </button>
+        <button type="button" class="btn-primary" @click="getRecommendation" :disabled="!seeds.length">
+          {{ $t('get_recommendations') }}
+        </button>
+      </div>
+    </div>
+
+    <div class="flex-1 flex-col overflow-hidden">
+      <ul v-if="!showRecommendation" class="h-full overflow-auto laptop:columns-2">
         <template v-if="searchType === 'artist'">
           <li v-for="artist in results.artists" :key="artist.id" class="flex items-center space-x-3 py-1 px-4">
             <img class="_cover" :src="spotifyCoverPicker(artist.images)" alt="" />
@@ -199,7 +223,7 @@ function getRecommendation() {
             </button>
           </li>
         </template>
-        <template v-if="searchType === 'track'">
+        <template v-else-if="searchType === 'track'">
           <li v-for="track in results.tracks" :key="track.id" class="flex items-center space-x-3 py-1 px-4">
             <img class="_cover" :src="spotifyCoverPicker(track.album.images)" alt="" />
             <div class="w-0 flex-1">
@@ -221,6 +245,7 @@ function getRecommendation() {
           </li>
         </template>
       </ul>
+      <TrackListContainer v-else class="h-full" :list="recommendationList" />
     </div>
   </div>
 </template>
