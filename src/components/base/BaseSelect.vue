9cecb01721a655a87e473853886a0113287cefeb
G100 <g100my.lan229@gmail.com>
2023-04-29 14:10:59 +0800

    style: refactor BaseSelect

--- src/components/base/BaseSelect.vue
+++ src/components/base/BaseSelect.vue
@@ -1,33 +1,15 @@
-<script lang="ts">
+<script lang="ts" setup>
 import { ref } from 'vue'
 import { Listbox, ListboxButton, ListboxOptions, ListboxOption, TransitionChild, TransitionRoot } from '@headlessui/vue'
 import IconDropdown from '@/assets/icons/icon-dropdown.svg?component'
 import { useI18n } from 'vue-i18n'
 
-export default {
-  components: {
-    Listbox,
-    ListboxButton,
-    ListboxOptions,
-    ListboxOption,
-    IconDropdown,
-    TransitionChild,
-    TransitionRoot,
-  },
-  props: {
-    options: {
-      type: Array,
-      required: true,
-    },
-  },
-  setup() {
-    const { t } = useI18n()
-    const placeholder = { id: null, name: t('choose_one') }
-    return {
-      seleted: ref(placeholder),
-    }
-  },
-}
+defineProps<{
+  options: { id: number | string; name: string }[]
+}>()
+
+const { t } = useI18n()
+const seleted = ref({ id: null, name: t('choose_one') })
 </script>
 <template>
   <Listbox v-slot="{ open }" v-model="seleted" as="div" class="relative max-w-lg">
@@ -61,18 +43,12 @@ export default {
         <ListboxOptions
           class="fixed inset-y-1/4 inset-x-0 z-30 mx-auto max-w-xs -translate-y-1 space-y-0.5 overflow-y-auto rounded rounded-b border border-t-0 border-tertiary-2 bg-tertiary-1 pt-1 pb-2 text-natural-gray3 laptop:absolute laptop:inset-auto laptop:mx-auto laptop:max-h-32 laptop:w-full laptop:max-w-none"
         >
-          <ListboxOption
-            v-for="(option, index) in options"
-            v-slot="{ selected }"
-            :key="index"
-            :value="option as Record<string, any>"
-            as="template"
-          >
+          <ListboxOption v-for="option in options" v-slot="{ selected }" :key="option.id" :value="option" as="template">
             <li
               class="w-full cursor-pointer overflow-hidden overflow-ellipsis whitespace-nowrap px-4 hover:bg-tertiary-2"
               :class="{ 'text-primary': selected }"
             >
-              {{ (option as Record<string, any>).name }}
+              {{ option.name }}
             </li>
           </ListboxOption>
         </ListboxOptions>
