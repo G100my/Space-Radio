1720660040dcde5ad5dc698f8ba17d43d24a9fe7
G100 <g100mylan229@gmail.com>
2024-06-09 11:56:27 +0800

    update: Search

--- apps/akijo/src/stores/useSearchStore.ts
+++ apps/akijo/src/stores/useSearchStore.ts
@@ -4,44 +4,96 @@ import { spotifyAPI } from '@/plugins/spotifyAPI'
 const localStorageKey = 'akijo-search'
 
 interface State {
+  query: string
   recentSearch: string[]
-  result: SpotifyApi.SearchResponse | null
+  tracksPaging: Omit<SpotifyApi.PagingObject<any>, 'items'> | null
+  tracks: SpotifyApi.TrackObjectFull[] | null
+  albumsPaging: Omit<SpotifyApi.PagingObject<any>, 'items'> | null
+  albums: SpotifyApi.AlbumObjectSimplified[] | null
+  artistsPaging: Omit<SpotifyApi.PagingObject<any>, 'items'> | null
+  artists: SpotifyApi.ArtistObjectFull[] | null
   type: Exclude<Parameters<typeof spotifyAPI.search>[1][number], 'playlist'>
 }
 export default defineStore('search', {
   state: (): State => ({
+    query: '',
     recentSearch: [],
     type: 'track',
-    result: null,
+    tracksPaging: null,
+    tracks: null,
+    albumsPaging: null,
+    albums: null,
+    artistsPaging: null,
+    artists: null,
   }),
   getters: {
-    listBySearchType: state => {
+    currentResult: state => {
       switch (state.type) {
         case 'album':
-          return state.result?.albums?.items || []
+          return { paging: state.albumsPaging, items: state.albums }
         case 'artist':
-          return state.result?.artists?.items || []
+          return { paging: state.artistsPaging, items: state.artists }
         case 'track':
-          return state.result?.tracks?.items || []
-        default:
-          return []
+          return { paging: state.tracksPaging, items: state.tracks }
       }
     },
   },
   actions: {
-    search(query: string) {
-      this.recentSearch.push(query)
+    search() {
+      this.recentSearch.push(this.query)
       if (this.recentSearch.length > 30) this.recentSearch.shift()
       localStorage.setItem(localStorageKey, JSON.stringify(this.recentSearch))
-      spotifyAPI.search(query, ['album', 'artist', 'track'], { market: 'TW' }).then(res => {
-        this.result = res
+      spotifyAPI.search(this.query, [this.type], { market: 'TW' }).then(res => {
+        switch (this.type) {
+          case 'album': {
+            const { items, ...rest } = (res as SpotifyApi.AlbumSearchResponse).albums
+            this.albums = items
+            this.albumsPaging = rest
+            break
+          }
+          case 'artist': {
+            const { items, ...rest } = (res as SpotifyApi.ArtistSearchResponse).artists
+            this.artists = items
+            this.artistsPaging = rest
+            break
+          }
+          case 'track': {
+            const { items, ...rest } = (res as SpotifyApi.TrackSearchResponse).tracks
+            this.tracks = items
+            this.tracksPaging = rest
+            break
+          }
+        }
       })
     },
-    getAlbum(id: string) {
-      return spotifyAPI.getAlbum(id)
-    },
-    getArtist(id: string) {
-      return spotifyAPI.getArtist(id)
+    async fetchNext() {
+      if (!this.currentResult.paging?.next) return Promise.resolve()
+      const limit = 25
+      const result = await spotifyAPI.search(this.query, [this.type], {
+        offset: this.currentResult.paging.offset + limit,
+        limit,
+      })
+      switch (this.type) {
+        case 'album': {
+          const { items, ...rest } = (result as SpotifyApi.AlbumSearchResponse).albums
+          this.albums = this.albums?.concat(items) ?? items
+          this.albumsPaging = rest
+          break
+        }
+        case 'artist': {
+          const { items, ...rest } = (result as SpotifyApi.ArtistSearchResponse).artists
+          this.artists = this.artists?.concat(items) ?? items
+          this.artistsPaging = rest
+          break
+        }
+        case 'track': {
+          const { items, ...rest } = (result as SpotifyApi.TrackSearchResponse).tracks
+          this.tracks = this.tracks?.concat(items) ?? items
+          this.tracksPaging = rest
+          break
+        }
+      }
+      return this.currentResult
     },
   },
 })
--- apps/akijo/src/views/customer/SearchMusic.vue
+++ apps/akijo/src/views/customer/SearchMusic.vue
@@ -1,13 +1,24 @@
 <script setup lang="ts">
-import { IconWrapper, BaseRadio } from 'shared'
-import { ref } from 'vue'
+import { IconWrapper, BaseRadio, useInfinityScroll } from 'shared'
+import { computed, ref } from 'vue'
 import { useSearchStore } from '@/stores'
 import TrackItem from '@/components/TrackItem.vue'
 import AlbumItem from '@/components/AlbumItem.vue'
 import ArtistItem from '@/components/ArtistItem.vue'
 
 const searchStore = useSearchStore()
-const keyword = ref('')
+const innerQuery = ref('')
+
+const { isLoading } = useInfinityScroll({
+  containerID: 'search_result_container',
+  nextCondition: computed(() => !!searchStore.currentResult?.paging?.next),
+  fetchCallback: searchStore.fetchNext,
+})
+
+function handleSearch() {
+  searchStore.query = innerQuery.value
+  searchStore.search()
+}
 
 function handleAdd() {
   // !
@@ -19,13 +30,8 @@ function handleAdd() {
     <div>
       <div class="mx-auto flex w-fit items-center gap-2">
         <label>關鍵字: </label>
-        <input
-          v-model="keyword"
-          type="text"
-          class="_input bg-tertiary-1"
-          @keydown.enter="searchStore.search(keyword)"
-        />
-        <button type="button" class="flex w-10 items-center justify-center" @click="searchStore.search(keyword)">
+        <input v-model="innerQuery" type="text" class="_input bg-tertiary-1" @keydown.enter="handleSearch" />
+        <button type="button" class="flex w-10 items-center justify-center" @click="handleSearch">
           <IconWrapper name="search-line" class="text-2xl" />
         </button>
       </div>
@@ -37,35 +43,35 @@ function handleAdd() {
       <BaseRadio name="searchType" v-model="searchStore.type" value="album" label="Album" />
     </div>
 
-    <div class="flex-1 overflow-auto px-14">
-      <p class="mb-3 flex items-baseline gap-3 text-xl font-bold">
+    <div class="flex flex-1 flex-col overflow-hidden">
+      <p class="sticky top-0 mb-3 flex items-baseline gap-3 px-14 text-xl font-bold">
         <span class="capitalize">{{ searchStore.type }}</span
         ><span class="h-px flex-1 bg-slate-400" />
       </p>
-      <div v-if="searchStore.listBySearchType">
-        <ul class="space-y-5">
-          <li v-for="i in searchStore.listBySearchType" :key="i.id" class="relative" @click="handleAdd">
-            <TrackItem
-              v-if="searchStore.type === 'track'"
-              :data="i as SpotifyApi.TrackObjectFull"
-              class="w-full overflow-hidden"
-            />
-            <AlbumItem
-              v-else-if="searchStore.type === 'album'"
-              :data="i as SpotifyApi.AlbumObjectFull"
-              class="w-full overflow-hidden"
-            />
-            <ArtistItem
-              v-else-if="searchStore.type === 'artist'"
-              :data="i as SpotifyApi.ArtistObjectFull"
-              class="w-full overflow-hidden"
-            />
-            <button type="button" class="absolute inset-y-0 -right-6" @click="handleAdd">
+      <ul id="search_result_container" class="flex-1 space-y-5 overflow-auto px-8 pb-16">
+        <li v-for="i in searchStore.currentResult?.items" :key="i.id" class="flex gap-3">
+          <TrackItem
+            v-if="searchStore.type === 'track'"
+            :data="i as SpotifyApi.TrackObjectFull"
+            class="w-full overflow-hidden"
+          />
+          <AlbumItem
+            v-else-if="searchStore.type === 'album'"
+            :data="i as SpotifyApi.AlbumObjectFull"
+            class="w-full overflow-hidden"
+          />
+          <ArtistItem
+            v-else-if="searchStore.type === 'artist'"
+            :data="i as SpotifyApi.ArtistObjectFull"
+            class="w-full overflow-hidden"
+          />
+          <div class="space-x-2">
+            <button type="button" @click="handleAdd">
               <IconWrapper name="add-line" class="text-2xl" />
             </button>
-          </li>
-        </ul>
-      </div>
+          </div>
+        </li>
+      </ul>
     </div>
   </section>
 </template>
