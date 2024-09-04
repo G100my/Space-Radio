af8cd10c774994834c9c01e7ffffe1ccef7a3040
G100 <g100mylan229@gmail.com>
2024-09-04 20:19:02 +0800

    fix(host): QueueList scroll bar

--- apps/host/src/views/QueueList.vue
+++ apps/host/src/views/QueueList.vue
@@ -13,29 +13,31 @@ async function handleResolve(params: { key: string; action: 'approve' | 'reject'
 }
 </script>
 <template>
-  <section class="flex-1 overflow-auto">
-    <ul v-if="hostStore.queueAmount" class="w-full space-y-3">
-      <li v-for="(i, key) in hostStore.queue" :key="key" class="flex items-center gap-3">
-        <div class="flex w-0 flex-1 items-center gap-2">
-          <p class="text-2xl">{{ i.site ?? '--' }}</p>
-          <TrackItem :data="i" class="flex-1 overflow-hidden" @play="audioStore.toggle(i)" />
-        </div>
-        <button
-          type="button"
-          class="aspect-square h-10 w-10 flex-shrink-0 rounded border-2"
-          @click="handleResolve({ key: key as string, action: 'approve' })"
-        >
-          <IconWrapper name="check-line" />
-        </button>
-        <button
-          type="button"
-          class="aspect-square h-10 w-10 flex-shrink-0 rounded border-2"
-          @click="handleResolve({ key: key as string, action: 'reject' })"
-        >
-          <IconWrapper name="close-line" />
-        </button>
-      </li>
-    </ul>
+  <section class="flex-1 overflow-visible">
+    <div v-if="hostStore.queueAmount" class="-mx-5 h-full overflow-auto">
+      <ul class="w-full space-y-3 px-5">
+        <li v-for="(i, key) in hostStore.queue" :key="key" class="flex items-center gap-3">
+          <div class="flex w-0 flex-1 items-center gap-2">
+            <p class="text-2xl">{{ i.site ?? '--' }}</p>
+            <TrackItem :data="i" class="flex-1 overflow-hidden" @play="audioStore.toggle(i)" />
+          </div>
+          <button
+            type="button"
+            class="aspect-square h-10 w-10 flex-shrink-0 rounded border-2"
+            @click="handleResolve({ key: key as string, action: 'approve' })"
+          >
+            <IconWrapper name="check-line" />
+          </button>
+          <button
+            type="button"
+            class="aspect-square h-10 w-10 flex-shrink-0 rounded border-2"
+            @click="handleResolve({ key: key as string, action: 'reject' })"
+          >
+            <IconWrapper name="close-line" />
+          </button>
+        </li>
+      </ul>
+    </div>
     <div v-else class="flex h-full items-center justify-center">沒人點歌...ＱＱ</div>
   </section>
 </template>
