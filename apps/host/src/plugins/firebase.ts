6b2382d6f295cf0dc45939f3594e653d844b481c
G100 <g100mylan229@gmail.com>
2024-09-07 11:12:04 +0800

    style: remove unnecessary

--- apps/host/src/plugins/firebase.ts
+++ apps/host/src/plugins/firebase.ts
@@ -10,14 +10,14 @@ import { getMessaging } from 'firebase/messaging'
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: 'AIzaSyA8QD5Shdlh3fW1oMeGe8XwiVIgsCOM328',
-  authDomain: 'jukebox-g100.firebaseapp.com',
-  databaseURL: 'https://akijo-space.asia-southeast1.firebasedatabase.app/',
-  projectId: 'jukebox-g100',
-  storageBucket: 'jukebox-g100.appspot.com',
-  messagingSenderId: '1011719496683',
-  appId: '1:1011719496683:web:fe50ae6d689d451f2a2fae',
-  measurementId: 'G-803MJPJYJ2',
+  apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTHDOMAIN,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 }
 
 // Initialize Firebase
