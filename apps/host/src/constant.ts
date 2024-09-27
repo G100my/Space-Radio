61cd087df551ab5adff62d8468ac0c64e57aa364
G100 <g100mylan229@gmail.com>
2024-09-27 18:21:47 +0800

    style(host): split firebaseConfig out

--- apps/host/src/constant.ts
+++ apps/host/src/constant.ts
@@ -13,3 +13,15 @@ export const routeMap = {
   Settings: 'settings',
   NotFound: '404',
 }
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTHDOMAIN,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+}
