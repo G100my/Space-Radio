b4bebefc0a39c668aa064893fe55fce780d679a3
G100 <g100mylan229@gmail.com>
2024-09-28 16:43:05 +0800

    fix(client): unknown reason - @inport oruga in index.css will broke tailwindcss

--- apps/client/src/main.ts
+++ apps/client/src/main.ts
@@ -1,10 +1,14 @@
-import './index.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import { router } from './router'
-import { OrugaConfig } from '@oruga-ui/oruga-next'
+
 import { GlobalComponentPlugin } from 'shared'
+
+import './index.css'
+
+import { OrugaConfig } from '@oruga-ui/oruga-next'
+import '@oruga-ui/theme-oruga/dist/oruga.min.css'
 
 const app = createApp(App)
 
