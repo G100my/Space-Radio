c00c6783cc45484c928d414ee8c368c8dd7407e6
G100 <g100mylan229@gmail.com>
2026-01-13 19:24:20 +0800

    replace author url

--- src/components/hall/HallNav.vue
+++ src/components/hall/HallNav.vue
@@ -3,6 +3,6 @@
     <a href="https://echowenchi.myportfolio.com/work" target="_blank"
       >Designer: <strong class="text-primary">Echo</strong></a
     >
-    <a href="https://g100my.github.io/" target="_blank">Developer: <strong class="text-primary">G100</strong></a>
+    <a href="https://g100.space" target="_blank">Developer: <strong class="text-primary">G100</strong></a>
   </div>
 </template>
